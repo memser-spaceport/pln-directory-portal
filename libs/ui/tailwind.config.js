4853d3f21a4de0b89c2ff72bb983b659b16ec851
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-04-11 17:27:49 +0100

    fix: update tailwind configuration to properly purge the apps and libs styles

--- libs/ui/tailwind.config.js
+++ libs/ui/tailwind.config.js
@@ -1,7 +1,11 @@
 const { join } = require('path');
+const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 
 module.exports = {
-  content: [join(__dirname, 'src/**/*.{js,ts,jsx,tsx}')],
+  content: [
+    join(__dirname, 'src/**/*.{js,ts,jsx,tsx}'),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
   theme: {
     extend: {},
   },
