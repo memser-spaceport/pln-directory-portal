3677a3bb1d0c6a44e2eadf26dbad227c832cc0f1
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-04-22 23:08:05 +0100

    feat: change airtable service to retrieve parsed teams and labbers

--- libs/airtable/src/models/team.ts
+++ libs/airtable/src/models/team.ts
@@ -6,34 +6,41 @@ export interface IAirtableTeam {
 }
 
 export interface IAirtableTeamFields {
-  Name: string;
-  'Short description': string;
-  'Long description': string;
-  Website: string;
-  Twitter: string;
-  'Funding Vehicle': string[];
-  Labbers: string[];
-  Logo: IAirtableTeamLogo[];
-  Industry: string[];
-  'Last Audited': Date;
-  Notes: string;
-  'Last Modified': Date;
-  'Eligible for marketplace credits': boolean;
-  'Grants program': boolean;
-  Blog: string;
+  Name?: string;
+  'Short description'?: string;
+  'Long description'?: string;
+  Website?: string;
+  Twitter?: string;
+  'Funding Vehicle'?: string[];
+  'Network members'?: string[];
+  Logo?: IAirtableTeamLogo[];
+  Industry?: string[];
+  'Last Audited'?: Date;
+  Notes?: string;
+  'Last Modified'?: Date;
+  'Eligible for marketplace credits'?: boolean;
+  'Grants program'?: boolean;
+  Blog?: string;
+  'IPFS User'?: boolean;
+  'Filecoin User'?: boolean;
+  Created?: string;
+  Video?: string;
+  'Funding Stage'?: string;
+  'Accelerator Programs'?: string[];
+  'Friend of PLN'?: boolean;
 }
 
 export interface IAirtableTeamLogo {
   id: string;
-  width: number;
-  height: number;
-  url: string;
-  filename: string;
-  size: number;
-  type: string;
-  thumbnails: {
-    small: IAirtableImage;
-    large: IAirtableImage;
-    full: IAirtableImage;
+  width?: number;
+  height?: number;
+  url?: string;
+  filename?: string;
+  size?: number;
+  type?: string;
+  thumbnails?: {
+    small?: IAirtableImage;
+    large?: IAirtableImage;
+    full?: IAirtableImage;
   };
 }
