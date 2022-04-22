815ada42ea4adf051af5f72d8b1ebb4bfc94846c
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-04-22 16:59:10 +0100

    hides tags based on container's boundaries

--- libs/ui/src/lib/Tags/Tags.tsx
+++ libs/ui/src/lib/Tags/Tags.tsx
@@ -1,20 +1,49 @@
+import { useEffect, useRef } from 'react';
+
 export interface TagsProps {
   tagsList: string[];
 }
 
-export function Tags({ tagsList }: TagsProps) {
-  const renderTags = (arr: string[]) => {
-    return arr.map((item: string, index: number) => (
-      <div
-        key={index}
-        className="text-xs px-3 py-1 mr-2 mb-2 border rounded-full"
-      >
-        {item}
-      </div>
-    ));
-  };
+export const renderTags = (arr: string[]) => {
+  return arr.map((item: string, i: number) => (
+    <div key={i} className="text-xs px-3 py-1 mr-2 mb-2 border rounded-full">
+      {item}
+    </div>
+  ));
+};
 
-  return <div className="tagsContainer">{renderTags(tagsList)}</div>;
+export function Tags({ tagsList }: TagsProps) {
+  const containerRef = useRef<HTMLHeadingElement>(null);
+  useEffect(() => {
+    const rightLimit = containerRef.current
+      ? containerRef.current?.offsetLeft + containerRef.current?.offsetWidth
+      : null;
+    const childrenArr = containerRef.current
+      ? [].slice.call(containerRef.current.children)
+      : [];
+
+    childrenArr.map((el) => {
+      const isOnLimitX = el.offsetLeft + el.offsetWidth >= rightLimit;
+      if (isOnLimitX) {
+        el.style.display = 'none';
+      }
+      console.group();
+      console.log('rightLimit', rightLimit);
+      console.log('>>> el', el, el.offsetLeft, el.offsetWidth);
+      console.log('isOnLimit', isOnLimitX);
+      console.log('childrenArr', childrenArr);
+      console.groupEnd();
+    });
+
+    //offsetLeft offsetTop offsetWidth
+    //.5rem
+  }, []);
+
+  return (
+    <div className="flex" ref={containerRef}>
+      {renderTags(tagsList)}
+    </div>
+  );
 }
 
 export default Tags;
