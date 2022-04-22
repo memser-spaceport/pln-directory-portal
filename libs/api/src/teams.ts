58e0a799a1831b0c4b7bf4add8dbb354be5ea28f
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-04-22 23:02:02 +0100

    feat: add lib for shared typescript interfaces

---
+++ libs/api/src/teams.ts
@@ -0,0 +1,15 @@
+export interface ITeam {
+  filecoinUser?: boolean;
+  fundingStage?: string;
+  fundingVehicle?: string[];
+  id: string;
+  industry?: string[];
+  ipfsUser?: boolean;
+  labbers?: string[];
+  logo?: string;
+  longDescription?: string;
+  name?: string;
+  shortDescription?: string;
+  twitter?: string;
+  website?: string;
+}
