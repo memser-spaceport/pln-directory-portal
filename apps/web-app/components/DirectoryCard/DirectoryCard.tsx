50dfe2a3e7101e3e61a1bd61f6d2c14b6e92d88c
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-04-27 15:44:43 +0100

    remove dynamic vars from classnames; changes id type; removes interface DirectoryCardProps

--- apps/web-app/components/DirectoryCard/DirectoryCard.tsx
+++ apps/web-app/components/DirectoryCard/DirectoryCard.tsx
@@ -1,14 +1,4 @@
-export interface DirectoryCardProps {
-  isGrid?: boolean;
-  clickEv?: any;
-  children: React.ReactChild | React.ReactChild[];
-}
-
-export function DirectoryCard({
-  isGrid,
-  clickEv,
-  children,
-}: DirectoryCardProps) {
+export function DirectoryCard({ isGrid, clickEv, children }) {
   return (
     <div
       className="bg-white border rounded-lg shadow-md text-sm px-6 pt-6 pb-4 min-w-[260px] cursor-pointer"
