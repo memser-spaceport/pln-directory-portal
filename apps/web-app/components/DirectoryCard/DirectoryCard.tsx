db982e06ae6092a2d0b61853f66e3799f1dae07b
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-04-28 17:00:17 +0100

    change card link

--- apps/web-app/components/DirectoryCard/DirectoryCard.tsx
+++ apps/web-app/components/DirectoryCard/DirectoryCard.tsx
@@ -2,18 +2,11 @@ import React from 'react';
 
 export type DirectoryCardProps = {
   isGrid?: boolean;
-} & Pick<React.ComponentPropsWithoutRef<'div'>, 'onClick' | 'children'>;
+} & Pick<React.ComponentPropsWithoutRef<'div'>, 'children'>;
 
-export function DirectoryCard({
-  isGrid,
-  onClick,
-  children,
-}: DirectoryCardProps) {
+export function DirectoryCard({ isGrid, children }: DirectoryCardProps) {
   return (
-    <div
-      className="w-[260px] bg-white border rounded-lg shadow-md text-sm  pb-4  cursor-pointer"
-      onClick={onClick}
-    >
+    <div className="w-[260px] bg-white border rounded-lg shadow-md text-sm  pb-4  cursor-pointer">
       {children}
     </div>
   );
