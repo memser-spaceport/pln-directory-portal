21bf1a6aece906a632a33bf0eae953afe017583e
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-05-17 12:44:58 +0100

    feat: optimize directory sort component and hook

--- apps/web-app/components/directory-sort/directory-sort.tsx
+++ apps/web-app/components/directory-sort/directory-sort.tsx
@@ -1,28 +1,32 @@
 import { SortAscendingIcon, SortDescendingIcon } from '@heroicons/react/solid';
 import { Dropdown } from '@protocol-labs-network/ui';
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import { DirectorySortButtonContent } from './directory-sort-button-content';
 import { IDirectorySortDropdownOption } from './directory-sort.types';
 import { useDirectorySortOption } from './use-directory-sort-option.hook';
 
+const DIRECTORY_SORT_DROPDOWN_OPTIONS: IDirectorySortDropdownOption[] = [
+  { label: 'A-Z', icon: SortDescendingIcon, value: 'Name,asc' },
+  { label: 'Z-A', icon: SortAscendingIcon, value: 'Name,desc' },
+];
+
 export function DirectorySort() {
   const { selectedDirectorySortOption, changeDirectorySortOption } =
     useDirectorySortOption();
+  const [directorySortDropdownOptions] = useState(
+    DIRECTORY_SORT_DROPDOWN_OPTIONS
+  );
 
-  const directorySortDropdownOptions: IDirectorySortDropdownOption[] = [
-    { label: 'A-Z', icon: SortDescendingIcon, value: 'Name,asc' },
-    { label: 'Z-A', icon: SortAscendingIcon, value: 'Name,desc' },
-  ];
+  const onDropdownOptionChange = useCallback(
+    (directorySortDropdownOption: IDirectorySortDropdownOption) => {
+      changeDirectorySortOption(directorySortDropdownOption.value);
+    },
+    [changeDirectorySortOption]
+  );
 
   const selectedDirectorySortDropdownOption = directorySortDropdownOptions.find(
     (option) => option.value === selectedDirectorySortOption
   );
-
-  function onDropdownOptionChange(
-    directorySortDropdownOption: IDirectorySortDropdownOption
-  ) {
-    changeDirectorySortOption(directorySortDropdownOption.value);
-  }
 
   return (
     <Dropdown
