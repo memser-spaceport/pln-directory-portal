5e5497124103c329b677a04ca05bdf2af306f4fd
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-05-26 16:27:04 +0100

    add placeholder when it doesn't exists a logo

--- apps/web-app/components/team-profile/team-profile-sidebar/team-profile-sidebar.tsx
+++ apps/web-app/components/team-profile/team-profile-sidebar/team-profile-sidebar.tsx
@@ -5,15 +5,21 @@ import { SocialLinks } from '../../social-links/social-links';
 export default function TeamProfileSidebar({ team }) {
   return (
     <div className="w-1/3 bg-white shadow-slate-200 px-6 py-9 border rounded-lg shadow-md text-sm cursor-default">
-      <div className="w-24 h-24 mb-4">
-        <Image
-          alt={`${team.name} Logo`}
-          src={team.logo}
-          width="100%"
-          height="100%"
-          layout="responsive"
-          objectFit="contain"
-        />
+      <div
+        className={`w-24 h-24 mb-4 rounded-lg ${
+          team.logo ? '' : 'bg-slate-200'
+        }`}
+      >
+        {team.logo ? (
+          <Image
+            alt={`${team.name} Logo`}
+            src={team.logo}
+            width="100%"
+            height="100%"
+            layout="responsive"
+            objectFit="contain"
+          />
+        ) : null}
       </div>
       <h3 className="text-base font-bold mb-4">{team.name}</h3>
       <div className="mb-16">{team.shortDescription}</div>
