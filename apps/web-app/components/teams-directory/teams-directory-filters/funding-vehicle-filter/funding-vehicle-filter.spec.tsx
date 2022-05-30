9dd6a397562c104d29e7651ba7c3ca81ee846c24
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-05-30 18:20:56 +0100

    feat: add funding vehicle filter

---
+++ apps/web-app/components/teams-directory/teams-directory-filters/funding-vehicle-filter/funding-vehicle-filter.spec.tsx
@@ -0,0 +1,39 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { IFilterTag } from '../../../../components/directory/directory-filters/directory-tags-filter/directory-tags-filter.types';
+import { createMockRouter } from '../../../../utils/test/createMockRouter';
+import FundingVehicleFilter from './funding-vehicle-filter';
+
+const tags: IFilterTag[] = [
+  {
+    value: 'Tag 01',
+    selected: false,
+    disabled: false,
+  },
+  {
+    value: 'Tag 02',
+    selected: false,
+    disabled: false,
+  },
+];
+
+describe('FundingVehicleFilter', () => {
+  it('should call the router push method with the selected options when a tag gets clicked', () => {
+    const push = jest.fn();
+
+    render(
+      <RouterContext.Provider value={createMockRouter({ push, query: {} })}>
+        <FundingVehicleFilter fundingVehicleTags={tags} />
+      </RouterContext.Provider>
+    );
+
+    const tag = screen.getByText('Tag 01');
+    fireEvent.click(tag);
+
+    expect(push).toHaveBeenCalledTimes(1);
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/',
+      query: { fundingVehicle: 'Tag 01' },
+    });
+  });
+});
