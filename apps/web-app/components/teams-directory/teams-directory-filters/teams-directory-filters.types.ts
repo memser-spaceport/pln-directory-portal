9067892fb94c9e5e0ca41bda4bf605156f3fd8ca
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-05-31 18:00:55 +0100

    feat: add teams directory filters utils

---
+++ apps/web-app/components/teams-directory/teams-directory-filters/teams-directory-filters.types.ts
@@ -0,0 +1,7 @@
+import { IFilterTag } from '../../../components/directory/directory-filters/directory-tags-filter/directory-tags-filter.types';
+
+export interface ITeamsFiltersValues {
+  fundingStage: IFilterTag[];
+  fundingVehicle: IFilterTag[];
+  industry: IFilterTag[];
+}
---
+++ apps/web-app/components/teams-directory/teams-directory-filters/teams-directory-filters.utils.spec.ts
@@ -0,0 +1,49 @@
+import { parseTeamsFilters } from './teams-directory-filters.utils';
+
+describe('#parseTeamsFilters', () => {
+  it('should parse teams filter values into each filter component`s consumable format', () => {
+    expect(
+      parseTeamsFilters(
+        {
+          valuesByFilter: {
+            industry: ['Industry 01', 'Industry 02', 'Industry 03'],
+            fundingVehicle: [
+              'Funding Vehicle 01',
+              'Funding Vehicle 02',
+              'Funding Vehicle 03',
+            ],
+            fundingStage: [
+              'Funding Stage 01',
+              'Funding Stage 02',
+              'Funding Stage 03',
+            ],
+          },
+          availableValuesByFilter: {
+            industry: ['Industry 01', 'Industry 03'],
+            fundingVehicle: ['Funding Vehicle 01', 'Funding Vehicle 03'],
+            fundingStage: ['Funding Stage 01', 'Funding Stage 02'],
+          },
+        },
+        {
+          industry: 'Industry 01',
+        }
+      )
+    ).toEqual({
+      industry: [
+        { value: 'Industry 01', selected: true, disabled: false },
+        { value: 'Industry 02', selected: false, disabled: true },
+        { value: 'Industry 03', selected: false, disabled: false },
+      ],
+      fundingVehicle: [
+        { value: 'Funding Vehicle 01', selected: false, disabled: false },
+        { value: 'Funding Vehicle 02', selected: false, disabled: true },
+        { value: 'Funding Vehicle 03', selected: false, disabled: false },
+      ],
+      fundingStage: [
+        { value: 'Funding Stage 01', selected: false, disabled: false },
+        { value: 'Funding Stage 02', selected: false, disabled: false },
+        { value: 'Funding Stage 03', selected: false, disabled: true },
+      ],
+    });
+  });
+});
