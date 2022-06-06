8f0df5f527dd3b0f1e7dea20245609205b5c50f1
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-06-06 10:16:54 +0100

    feat: add reusable badge component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -1,4 +1,5 @@
 export * from './lib/tag/tag';
+export * from './lib/badge/badge';
 export * from './lib/anchor-link/anchor-link';
 export * from './lib/dropdown/dropdown';
 export * from './lib/input-field/input-field';
