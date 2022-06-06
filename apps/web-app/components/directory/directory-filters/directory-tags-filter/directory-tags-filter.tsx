de53c26b21e471009aa51b6376a1e03e88acdf1d
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-06-06 10:17:31 +0100

    feat: add ability to collapse/expand tags filter options

--- apps/web-app/components/directory/directory-filters/directory-tags-filter/directory-tags-filter.tsx
+++ apps/web-app/components/directory/directory-filters/directory-tags-filter/directory-tags-filter.tsx
@@ -1,3 +1,7 @@
+import { ChevronDownIcon } from '@heroicons/react/solid';
+import { Badge, Tag } from '@protocol-labs-network/ui';
+import * as Collapsible from '@radix-ui/react-collapsible';
+import { useState } from 'react';
 import { IFilterTag } from '../../../../components/directory/directory-filters/directory-tags-filter/directory-tags-filter.types';
 import DirectoryFilter from '../directory-filter/directory-filter';
 
@@ -7,29 +11,46 @@ export interface DirectoryTagsFilterProps {
   onTagToggle?: (index: number) => void;
 }
 
+const VISIBLE_TAGS_COUNT = 10;
+
 function DirectoryTagsFilter({
   title,
   tags,
   onTagToggle,
 }: DirectoryTagsFilterProps) {
+  const visibleTags = tags.slice(0, VISIBLE_TAGS_COUNT);
+  const collapsibleTags = tags.slice(VISIBLE_TAGS_COUNT);
+  const [open, setOpen] = useState(false);
+
   return (
     <DirectoryFilter title={title}>
-      {tags.map((tag, index) => (
-        <button
-          key={index}
-          className={`text-xs px-3 py-1 mr-2 mb-2 last:mr-0 border rounded-full hover:text-sky-700 focus:outline-none focus:ring focus:ring-sky-300/30 focus:border-sky-300 ${
-            tag.selected
-              ? 'border-sky-700 text-sky-700 bg-sky-100'
-              : tag.disabled
-              ? 'border-slate-200 text-slate-400 pointer-events-none'
-              : 'border-slate-300'
-          }`}
-          onClick={() => onTagToggle(index)}
-          disabled={tag.disabled}
-        >
-          {tag.value}
-        </button>
-      ))}
+      <Collapsible.Root open={open} onOpenChange={setOpen}>
+        {visibleTags.map((tag, index) => (
+          <Tag key={index} {...tag} onClick={() => onTagToggle(index)} />
+        ))}
+        {collapsibleTags.length ? (
+          <>
+            <Collapsible.Content className="inline">
+              {collapsibleTags.map((tag, index) => (
+                <Tag
+                  key={index + VISIBLE_TAGS_COUNT}
+                  {...tag}
+                  onClick={() => onTagToggle(index + VISIBLE_TAGS_COUNT)}
+                />
+              ))}
+            </Collapsible.Content>
+            <Collapsible.Trigger className="flex items-center mt-2">
+              <span className="text-xs font-semibold leading-4 mr-1">
+                Show {open ? 'less' : 'more'}
+              </span>
+              {open ? null : <Badge text={`${collapsibleTags.length}`} />}
+              <ChevronDownIcon
+                className={`h-5 ml-2 ${open ? 'rotate-180' : ''}`}
+              />
+            </Collapsible.Trigger>
+          </>
+        ) : null}
+      </Collapsible.Root>
     </DirectoryFilter>
   );
 }
