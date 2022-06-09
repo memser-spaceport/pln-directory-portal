eca8aed6e1deab36983abc6ab24720e3dfdb897a
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-06-09 10:11:45 +0100

    chore: update sentry configs

--- apps/web-app/sentry.client.config.ts
+++ apps/web-app/sentry.client.config.ts
@@ -1,13 +1,8 @@
 import * as Sentry from '@sentry/nextjs';
 
-process.env.NEXT_PUBLIC_SENTRY_DSN &&
-  Sentry.init({
-    dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-    // We recommend adjusting this value in production, or using tracesSampler
-    // for finer control
-    tracesSampleRate: 0.2,
-    // ...
-    // Note: if you want to override the automatic release value, do not set a
-    // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-    // that it will also get attached to your source maps
-  });
+Sentry.init({
+  enabled: !!process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
+  tracesSampleRate: 0.2,
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || 'development',
+});
