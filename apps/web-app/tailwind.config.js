359ae82155cf37765aebc03f1a6b59be23f11699
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-06-20 10:59:03 +0100

    chore: add tailwindcss preset file to be shared between apps and libs

--- apps/web-app/tailwind.config.js
+++ apps/web-app/tailwind.config.js
@@ -2,13 +2,10 @@ const { join } = require('path');
 const { createGlobPatternsForDependencies } = require('@nrwl/next/tailwind');
 
 module.exports = {
+  presets: [require('../../tailwind-workspace-preset.js')],
   content: [
     join(__dirname, 'pages/**/*.{js,ts,jsx,tsx}'),
     join(__dirname, 'components/**/*.{js,ts,jsx,tsx}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('@tailwindcss/line-clamp')],
 };
