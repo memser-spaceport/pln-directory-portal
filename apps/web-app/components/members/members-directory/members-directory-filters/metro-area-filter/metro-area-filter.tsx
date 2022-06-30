1c491d3b39dbca06274cc6f05fdd026a0f089161
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-06-30 15:55:51 +0100

    feat: add metro area filter

---
+++ apps/web-app/components/members/members-directory/members-directory-filters/metro-area-filter/metro-area-filter.tsx
@@ -0,0 +1,19 @@
+import DirectoryTagsFilter from '../../../../directory/directory-filters/directory-tags-filter/directory-tags-filter';
+import { IFilterTag } from '../../../../directory/directory-filters/directory-tags-filter/directory-tags-filter.types';
+import { useTagsFilter } from '../../../../directory/directory-filters/directory-tags-filter/use-tags-filter.hook';
+
+export interface MetroAreaFilterProps {
+  metroAreaTags: IFilterTag[];
+}
+
+export function MetroAreaFilter({ metroAreaTags }: MetroAreaFilterProps) {
+  const [tags, toggleTag] = useTagsFilter('metroArea', metroAreaTags);
+
+  return (
+    <DirectoryTagsFilter
+      title="MetroArea"
+      tags={tags}
+      onTagToggle={toggleTag}
+    />
+  );
+}
