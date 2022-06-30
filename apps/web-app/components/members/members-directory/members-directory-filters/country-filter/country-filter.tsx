633228d291eb440eb7fa57655580b1b8ac83bcf3
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-06-30 15:47:39 +0100

    feat: add country filter

---
+++ apps/web-app/components/members/members-directory/members-directory-filters/country-filter/country-filter.tsx
@@ -0,0 +1,15 @@
+import DirectoryTagsFilter from '../../../../directory/directory-filters/directory-tags-filter/directory-tags-filter';
+import { IFilterTag } from '../../../../directory/directory-filters/directory-tags-filter/directory-tags-filter.types';
+import { useTagsFilter } from '../../../../directory/directory-filters/directory-tags-filter/use-tags-filter.hook';
+
+export interface CountryFilterProps {
+  countryTags: IFilterTag[];
+}
+
+export function CountryFilter({ countryTags }: CountryFilterProps) {
+  const [tags, toggleTag] = useTagsFilter('country', countryTags);
+
+  return (
+    <DirectoryTagsFilter title="Country" tags={tags} onTagToggle={toggleTag} />
+  );
+}
