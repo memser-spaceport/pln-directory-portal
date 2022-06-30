e81e88bf32e818d214fcfd08dd251d16a438de5e
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-06-30 15:24:04 +0100

    feat: add skills filter

---
+++ apps/web-app/components/members/members-directory/members-directory-filters/skills-filter/skills-filter.spec.tsx
@@ -0,0 +1,39 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { createMockRouter } from '../../../../../utils/test/createMockRouter';
+import { IFilterTag } from '../../../../directory/directory-filters/directory-tags-filter/directory-tags-filter.types';
+import { SkillsFilter } from './skills-filter';
+
+const tags: IFilterTag[] = [
+  {
+    value: 'Tag 01',
+    selected: false,
+    disabled: false,
+  },
+  {
+    value: 'Tag 02',
+    selected: false,
+    disabled: false,
+  },
+];
+
+describe('SkillsFilter', () => {
+  it('should call the router push method with the selected options when a tag gets clicked', () => {
+    const push = jest.fn();
+
+    render(
+      <RouterContext.Provider value={createMockRouter({ push, query: {} })}>
+        <SkillsFilter skillsTags={tags} />
+      </RouterContext.Provider>
+    );
+
+    const tag = screen.getByText('Tag 01');
+    fireEvent.click(tag);
+
+    expect(push).toHaveBeenCalledTimes(1);
+    expect(push).toHaveBeenCalledWith({
+      pathname: '/',
+      query: { skills: 'Tag 01' },
+    });
+  });
+});
---
+++ apps/web-app/components/members/members-directory/members-directory-filters/skills-filter/skills-filter.tsx
@@ -0,0 +1,15 @@
+import DirectoryTagsFilter from '../../../../directory/directory-filters/directory-tags-filter/directory-tags-filter';
+import { IFilterTag } from '../../../../directory/directory-filters/directory-tags-filter/directory-tags-filter.types';
+import { useTagsFilter } from '../../../../directory/directory-filters/directory-tags-filter/use-tags-filter.hook';
+
+export interface SkillsFilterProps {
+  skillsTags: IFilterTag[];
+}
+
+export function SkillsFilter({ skillsTags }: SkillsFilterProps) {
+  const [tags, toggleTag] = useTagsFilter('skills', skillsTags);
+
+  return (
+    <DirectoryTagsFilter title="Skills" tags={tags} onTagToggle={toggleTag} />
+  );
+}
