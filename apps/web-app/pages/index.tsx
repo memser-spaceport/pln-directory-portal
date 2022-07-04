730cbb4d14658983ff26fb4ef044ec1afc761bb6
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-07-04 17:04:18 +0100

    feat: adds a redirect on the initial page to teams

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -1,16 +1,12 @@
-export function Index() {
-  /*
-   * Replace the elements below with your own.
-   *
-   * Note: The corresponding styles are in the ./index.scss file.
-   */
-  return (
-    <div>
-      <h1 className="text-3xl font-extrabold tracking-tight text-gray-900">
-        Protocol Labs Network
-      </h1>
-    </div>
-  );
+export default function Index() {
+  return <div></div>;
 }
 
-export default Index;
+export const getServerSideProps = async () => {
+  return {
+    redirect: {
+      permanent: false,
+      destination: '/teams',
+    },
+  };
+};
