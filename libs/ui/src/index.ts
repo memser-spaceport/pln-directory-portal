5c2296e48d212b8f8e9b5473432a2d4c6593cfa2
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-07-18 16:05:31 +0100

    feat: add spinner icon component

--- libs/ui/src/index.ts
+++ libs/ui/src/index.ts
@@ -4,6 +4,7 @@ export * from './lib/badge/badge';
 export * from './lib/breadcrumb/breadcrumb';
 export * from './lib/dropdown/dropdown';
 export * from './lib/icons/arrow/arrow';
+export * from './lib/icons/spinner/spinner';
 export * from './lib/input-field/input-field';
 export * from './lib/tag/tag';
 export * from './lib/tooltip/tooltip';
