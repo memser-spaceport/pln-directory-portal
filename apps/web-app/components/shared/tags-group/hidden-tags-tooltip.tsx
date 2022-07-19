5c0f7fb3b105f0886a68a041eeb007215b7af66e
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-07-19 15:16:33 +0100

    feat: change tags styles

--- apps/web-app/components/shared/tags-group/hidden-tags-tooltip.tsx
+++ apps/web-app/components/shared/tags-group/hidden-tags-tooltip.tsx
@@ -6,7 +6,7 @@ export interface HiddenTagsTooltipProps {
 
 export function HiddenTagsTooltip({ items }: HiddenTagsTooltipProps) {
   const hiddenTagsTrigger = () => (
-    <span className="tag tag--clickable group flex h-7 w-7 cursor-pointer items-center justify-center rounded-full">
+    <span className="tag group flex h-7 w-7 cursor-pointer items-center justify-center rounded-full hover:bg-slate-200 hover:text-slate-900">
       +{items.length}
     </span>
   );
--- libs/ui/src/lib/badge/badge.tsx
+++ libs/ui/src/lib/badge/badge.tsx
@@ -4,7 +4,7 @@ export interface BadgeProps {
 
 export function Badge({ text }: BadgeProps) {
   return (
-    <div className="rounded-3xl bg-slate-100 py-0.5 px-2 text-xs font-medium leading-4 text-slate-600">
+    <div className="rounded-3xl bg-slate-100 py-0.5 px-2 text-xs font-medium leading-[1.17] text-slate-600">
       {text}
     </div>
   );
