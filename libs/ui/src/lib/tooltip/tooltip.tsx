d88023cfce8bb1593a172b944d865ee99e9151bd
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-07-21 09:56:58 +0100

    fix: replace radix's hover card with tooltip on tooltip ui component

--- libs/ui/src/lib/tooltip/tooltip.tsx
+++ libs/ui/src/lib/tooltip/tooltip.tsx
@@ -1,4 +1,4 @@
-import * as HoverCard from '@radix-ui/react-hover-card';
+import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 
 export interface TooltipProps {
   Trigger: React.ElementType;
@@ -7,18 +7,20 @@ export interface TooltipProps {
 
 export function Tooltip({ Trigger, children }: TooltipProps) {
   return (
-    <HoverCard.Root openDelay={0} closeDelay={0}>
-      <HoverCard.Trigger className="cursor-pointer">
-        <Trigger />
-      </HoverCard.Trigger>
-      <HoverCard.Content
-        side="top"
-        align="center"
-        sideOffset={8}
-        className="flex-shrink-0 rounded bg-slate-900 py-1 px-2 text-xs font-medium text-white"
-      >
-        {children}
-      </HoverCard.Content>
-    </HoverCard.Root>
+    <TooltipPrimitive.Provider delayDuration={0}>
+      <TooltipPrimitive.Root>
+        <TooltipPrimitive.Trigger className="cursor-pointer">
+          <Trigger />
+        </TooltipPrimitive.Trigger>
+        <TooltipPrimitive.Content
+          side="top"
+          align="center"
+          sideOffset={8}
+          className="flex-shrink-0 rounded bg-slate-900 py-1 px-2 text-xs font-medium text-white"
+        >
+          {children}
+        </TooltipPrimitive.Content>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
   );
 }
