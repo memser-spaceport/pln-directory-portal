a8ef3603390c4cbe71913537c04f6b1ee85b48a9
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-07-26 22:48:12 +0100

    feat: update teams cards

---
+++ apps/web-app/components/directory/directory-card/directory-card-header.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import { TeamLeadBadge } from '../../shared/members/team-lead-badge/team-lead-badge';
+
+export interface DirectoryCardHeaderProps {
+  isGrid?: boolean;
+  isImageRounded?: boolean;
+  img?: string;
+  name: string;
+  avatarIcon: (props: React.ComponentProps<'svg'>) => JSX.Element;
+  teamLead?: boolean;
+}
+
+export function DirectoryCardHeader({
+  isGrid = true,
+  isImageRounded,
+  img,
+  name,
+  avatarIcon,
+  teamLead,
+}: DirectoryCardHeaderProps) {
+  const Icon = avatarIcon;
+
+  return (
+    <>
+      <div
+        className={`relative h-[72px] w-[72px] shrink-0 border border-slate-200 ${
+          isImageRounded ? 'rounded-full' : 'rounded-lg'
+        } ${isGrid ? 'mx-auto' : ''} ${img ? 'bg-white' : 'bg-slate-200'}`}
+      >
+        {img ? (
+          <Image
+            className={isImageRounded ? 'rounded-full' : 'rounded-lg'}
+            alt={`${name} img`}
+            src={img}
+            layout="fill"
+            objectFit={isImageRounded ? 'cover' : 'contain'}
+            objectPosition="center"
+          />
+        ) : (
+          <Icon className="w-22 h-22 mt-2 fill-white" />
+        )}
+        {teamLead ? (
+          <div className="absolute top-0 right-1 z-10">
+            <TeamLeadBadge />
+          </div>
+        ) : null}
+      </div>
+    </>
+  );
+}
--- apps/web-app/components/shared/members/member-card/member-card.tsx
+++ apps/web-app/components/shared/members/member-card/member-card.tsx
@@ -1,12 +1,10 @@
 import { LocationMarkerIcon } from '@heroicons/react/outline';
 import { UserIcon } from '@heroicons/react/solid';
 import { IMember } from '@protocol-labs-network/api';
-import { AnchorLink } from '@protocol-labs-network/ui';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { DirectoryCard } from '../../../../components/directory/directory-card/directory-card';
-import { TeamLeadBadge } from '../../../../components/shared/members/team-lead-badge/team-lead-badge';
-import { TagsGroup } from '../../tags-group/tags-group';
+import { DirectoryCardFooter } from '../../../../components/directory/directory-card/directory-card-footer';
+import { DirectoryCardHeader } from '../../../../components/directory/directory-card/directory-card-header';
 
 interface MemberCardProps {
   isGrid?: boolean;
@@ -19,53 +17,37 @@ export function MemberCard({ isGrid = true, member }: MemberCardProps) {
   const teamsNames = member.teams.map(({ name }) => name);
 
   return (
-    <DirectoryCard isGrid={isGrid}>
-      <AnchorLink href={`/members/${member.id}?backLink=${backLink}`}>
-        <div className={`flex ${isGrid ? 'flex-col space-y-4' : 'flex-row'}`}>
-          <div className={`${isGrid ? 'w-full' : 'w-[396px]'} flex space-x-4`}>
-            <div className="relative flex-shrink-0">
-              <div
-                className={`h-20 w-20 overflow-hidden rounded-full ${
-                  member.image ? '' : 'bg-slate-200'
-                }`}
-              >
-                {member.image ? (
-                  <Image
-                    className="rounded-full"
-                    alt={`${member.name} Logo`}
-                    src={member.image}
-                    layout="fill"
-                    objectFit="cover"
-                    objectPosition="center"
-                  />
-                ) : (
-                  <UserIcon className="w-22 h-22 mt-2 fill-white" />
-                )}
-              </div>
-
-              {member.teamLead ? (
-                <div className="absolute top-0 right-1 z-10">
-                  <TeamLeadBadge />
-                </div>
-              ) : null}
-            </div>
-            <div>
-              <h3 className="line-clamp-1 text-base font-semibold text-slate-900">
-                {member.name}
-              </h3>
-              <p className="line-clamp-1">{member.role}</p>
-              <div className="mt-2 flex items-center text-sm text-slate-500">
-                <LocationMarkerIcon className="mr-1 h-4 w-4 flex-shrink-0" />
-                <span className="line-clamp-1">{member.location}</span>
-              </div>
-            </div>
+    <DirectoryCard
+      isGrid={isGrid}
+      cardUrl={`/members/${member.id}?backLink=${backLink}`}
+    >
+      <DirectoryCardHeader
+        isGrid={isGrid}
+        isImageRounded
+        img={member.image}
+        avatarIcon={UserIcon}
+        name={member.name}
+        teamLead={member.teamLead}
+      />
+      <div className={isGrid ? '' : 'w-[400px] grow-0'}>
+        <h2 className={`${isGrid ? 'my-2' : ''} text-lg font-semibold`}>
+          {member.name}
+        </h2>
+        <p className="line-clamp-1">{member.role}</p>
+        {member.location ? (
+          <div
+            className={`mt-2 flex items-center text-sm text-slate-500 ${
+              isGrid ? 'justify-center' : ''
+            }`}
+          >
+            <LocationMarkerIcon className="mr-1 h-4 w-4 flex-shrink-0" />
+            <span className="line-clamp-1">{member.location}</span>
           </div>
-        </div>
-        <div className={`${isGrid ? 'mt-4' : 'mx-4 w-[348px] self-center'}`}>
-          <h4 className="mb-2 text-sm font-medium text-slate-500">Teams</h4>
-          <TagsGroup items={teamsNames} isSingleLine={true} />
-        </div>
-      </AnchorLink>
+        ) : (
+          '-'
+        )}
+      </div>
+      <DirectoryCardFooter isGrid={isGrid} tagsArr={teamsNames} />
     </DirectoryCard>
   );
 }
