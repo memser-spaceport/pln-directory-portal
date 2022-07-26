00046c9ad99911d2e1a8bb2c2b3bc0a701142ddc
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-07-26 09:38:37 +0100

    feat: update ask to edit

--- tailwind-workspace-preset.js
+++ tailwind-workspace-preset.js
@@ -1,7 +1,15 @@
 module.exports = {
   theme: {
     extend: {
+      backgroundImage: {
+        ask_to_edit_card: "url('../public/assets/images/ask-to-edit-bg.png')",
+      },
       boxShadow: {
+        'card--slate-900': [
+          '0px 0px 1px rgba(15, 23, 42, 0.12)',
+          '0px 4px 4px rgba(15, 23, 42, 0.04)',
+        ],
+        'request-button': ['0px 1px 1px rgba(15, 23, 42, 0.08)'],
         'special-button-default': [
           '0px 1px 1px rgba(7, 8, 8, 0.16)',
           'inset 0px 1px 0px rgba(255, 255, 255, 0.16)',
