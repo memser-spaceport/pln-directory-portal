099effac790569a2184391c57f199e6104954b9e
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-07-27 15:10:58 +0100

    feat: add api route for getting paginated members

---
+++ apps/web-app/pages/api/members/index.ts
@@ -0,0 +1,77 @@
+import airtableService from '@protocol-labs-network/airtable';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { env } from 'process';
+import { getMembersDirectoryRequestParametersFromQuery } from '../../../utils/api/list.utils';
+
+export default async function getMembersHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') {
+    res.setHeader('Allow', ['GET']);
+    res.status(405).end(`🚫 Method ${req.method} Not Allowed`);
+
+    return;
+  }
+
+  try {
+    const params = getMembersDirectoryRequestParametersFromQuery(req.query);
+    let url = encodeURI(
+      `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_MEMBERS_TABLE_ID}?api_key=${env.AIRTABLE_API_KEY}&${params}`
+    );
+    let offset = req.query.offset;
+
+    // When offset is not provided, it's because we don't have it yet,
+    // and it means it is the first request.
+    // Therefore, we need to make a redundant request to the first page of
+    // members, so that we get the offset for the second page.
+    if (!offset) {
+      const airtableResponse = await fetch(url);
+      const data = await airtableResponse.json();
+
+      // When the response has no offset, it means there are no more pages
+      // available to be fetch, and therefore, we return an empty list of members
+      // that will be concatenated in already existing list of members.
+      if (!data.offset) {
+        // Cache response data in the browser for 1 minute,
+        // and in the CDN for 5 minutes, while keeping it stale for 7 days.
+        res.setHeader(
+          'Cache-Control',
+          'public, max-age=60, s-maxage=300, stale-while-revalidate=604800'
+        );
+
+        res.status(200).json({
+          offset: '',
+          members: [],
+        });
+
+        return;
+      }
+
+      // When there is an offset, we update our local variable so that we can
+      // request the second page of members, that will effectively be returned
+      // to our list component for concatenation.
+      offset = data.offset;
+    }
+
+    url += `&offset=${offset}`;
+
+    // Actual request for a new page of members.
+    const airtableResponse = await fetch(url);
+    const data = await airtableResponse.json();
+
+    // Cache response data in the browser for 1 minute,
+    // and in the CDN for 5 minutes, while keeping it stale for 7 days.
+    res.setHeader(
+      'Cache-Control',
+      'public, max-age=60, s-maxage=300, stale-while-revalidate=604800'
+    );
+
+    res.status(200).json({
+      offset: data.offset, // The offset for the next page.
+      members: airtableService.parseMembers(data.records),
+    });
+  } catch (error) {
+    res.status(500).json({ msg: 'Ups, something went wrong 😕' });
+  }
+}
---
+++ apps/web-app/tests/pages/api/members/index.spec.ts
@@ -0,0 +1,218 @@
+import airtableService from '@protocol-labs-network/airtable';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { env } from 'process';
+import getMembersHandler from '../../../../pages/api/members';
+import { getMembersDirectoryRequestParametersFromQuery } from '../../../../utils/api/list.utils';
+
+const mockMembers = {
+  records: [{ Name: 'member_01' }, { Name: 'member_02' }],
+  offset: 'offset',
+};
+const jsonMock: jest.Mock<
+  Promise<{
+    records: { [prop: string]: string }[];
+    offset?: string;
+  }>,
+  []
+> = jest.fn(() => Promise.resolve(mockMembers));
+(global.fetch as jest.Mock) = jest.fn(() =>
+  Promise.resolve({
+    json: jsonMock,
+  })
+);
+
+const mockParsedMembers = [{ name: 'member_01' }, { name: 'member_02' }];
+jest.mock('@protocol-labs-network/airtable', () => ({
+  parseMembers: jest.fn(() => mockParsedMembers),
+}));
+
+const mockQueryParamsString = 'mockQueryParamsString';
+jest.mock('../../../utils/api/list.utils', () => ({
+  getMembersDirectoryRequestParametersFromQuery: jest.fn(
+    () => mockQueryParamsString
+  ),
+}));
+
+describe('/api/members', () => {
+  const resJson = jest.fn();
+  const resEnd = jest.fn();
+  const resSetHeader = jest.fn();
+  const resStatus = jest.fn(() => ({ json: resJson, end: resEnd }));
+  const req: NextApiRequest = {
+    method: 'GET',
+    query: {},
+  } as unknown as NextApiRequest;
+  const res: NextApiResponse = {
+    status: resStatus,
+    setHeader: resSetHeader,
+  } as unknown as NextApiResponse;
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    (fetch as jest.Mock).mockClear();
+    await getMembersHandler(req, res);
+  });
+
+  describe('and the request method is not supported', () => {
+    let oldMethod: string;
+
+    beforeAll(() => {
+      oldMethod = req.method;
+      req.method = 'POST';
+    });
+
+    it('should respond with an error', () => {
+      expect(resSetHeader).toHaveBeenCalledTimes(1);
+      expect(resSetHeader).toHaveBeenCalledWith('Allow', ['GET']);
+      expect(resStatus).toHaveBeenCalledTimes(1);
+      expect(resStatus).toHaveBeenCalledWith(405);
+      expect(resEnd).toHaveBeenCalledTimes(1);
+      expect(resEnd).toHaveBeenCalledWith('🚫 Method POST Not Allowed');
+      expect(fetch).not.toHaveBeenCalled();
+    });
+
+    afterAll(() => {
+      req.method = oldMethod;
+    });
+  });
+
+  it('should parse the provided query parameters into a query parameters string', () => {
+    expect(getMembersDirectoryRequestParametersFromQuery).toHaveBeenCalledWith(
+      req.query
+    );
+  });
+
+  describe('and there is an offset in the provided query parameters', () => {
+    let oldQuery: { [key: string]: string | string[] };
+    const offsetMock = 'some_offset';
+
+    beforeAll(() => {
+      oldQuery = req.query;
+      req.query = { offset: offsetMock };
+    });
+
+    it('should only get a new page of members', () => {
+      expect(fetch).toHaveBeenCalledTimes(1);
+      expect(fetch).toHaveBeenCalledWith(
+        encodeURI(
+          `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_MEMBERS_TABLE_ID}?api_key=${env.AIRTABLE_API_KEY}&${mockQueryParamsString}&offset=${offsetMock}`
+        )
+      );
+      expect(jsonMock).toHaveBeenCalledTimes(1);
+      expect(resSetHeader).toHaveBeenCalledTimes(1);
+      expect(resSetHeader).toHaveBeenCalledWith(
+        'Cache-Control',
+        'public, max-age=60, s-maxage=300, stale-while-revalidate=604800'
+      );
+      expect(resStatus).toHaveBeenCalledTimes(1);
+      expect(resStatus).toHaveBeenCalledWith(200);
+      expect(airtableService.parseMembers).toHaveBeenCalledTimes(1);
+      expect(airtableService.parseMembers).toHaveBeenCalledWith(
+        mockMembers.records
+      );
+      expect(resJson).toHaveBeenCalledTimes(1);
+      expect(resJson).toHaveBeenCalledWith({
+        offset: mockMembers.offset,
+        members: mockParsedMembers,
+      });
+    });
+
+    afterAll(() => {
+      req.query = oldQuery;
+    });
+  });
+
+  describe('and there is no offset in the provided query parameters', () => {
+    describe('and there is an offset in the first request response', () => {
+      const mockMembers02 = {
+        records: [{ Name: 'member_03' }, { Name: 'member_04' }],
+        offset: 'offset_02',
+      };
+
+      beforeAll(() => {
+        jsonMock.mockReturnValueOnce(Promise.resolve(mockMembers));
+        jsonMock.mockReturnValueOnce(Promise.resolve(mockMembers02));
+      });
+
+      it('should only get a new page of members', () => {
+        expect(fetch).toHaveBeenCalledTimes(2);
+        expect(fetch).toHaveBeenNthCalledWith(
+          1,
+          encodeURI(
+            `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_MEMBERS_TABLE_ID}?api_key=${env.AIRTABLE_API_KEY}&${mockQueryParamsString}`
+          )
+        );
+        expect(fetch).toHaveBeenNthCalledWith(
+          2,
+          encodeURI(
+            `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_MEMBERS_TABLE_ID}?api_key=${env.AIRTABLE_API_KEY}&${mockQueryParamsString}&offset=${mockMembers.offset}`
+          )
+        );
+        expect(jsonMock).toHaveBeenCalledTimes(2);
+        expect(resSetHeader).toHaveBeenCalledTimes(1);
+        expect(resSetHeader).toHaveBeenCalledWith(
+          'Cache-Control',
+          'public, max-age=60, s-maxage=300, stale-while-revalidate=604800'
+        );
+        expect(resStatus).toHaveBeenCalledTimes(1);
+        expect(resStatus).toHaveBeenCalledWith(200);
+        expect(airtableService.parseMembers).toHaveBeenCalledTimes(1);
+        expect(airtableService.parseMembers).toHaveBeenCalledWith(
+          mockMembers02.records
+        );
+        expect(resJson).toHaveBeenCalledTimes(1);
+        expect(resJson).toHaveBeenCalledWith({
+          offset: mockMembers02.offset,
+          members: mockParsedMembers,
+        });
+      });
+    });
+
+    describe('and there is no offset in the first request response', () => {
+      const mockMembersWithoutOffset = {
+        records: mockMembers.records,
+      };
+
+      beforeAll(() => {
+        jsonMock.mockReturnValueOnce(Promise.resolve(mockMembersWithoutOffset));
+      });
+
+      it('should only get a new page of members', () => {
+        expect(fetch).toHaveBeenCalledTimes(1);
+        expect(fetch).toHaveBeenCalledWith(
+          encodeURI(
+            `https://api.airtable.com/v0/${env.AIRTABLE_BASE_ID}/${env.AIRTABLE_MEMBERS_TABLE_ID}?api_key=${env.AIRTABLE_API_KEY}&${mockQueryParamsString}`
+          )
+        );
+        expect(jsonMock).toHaveBeenCalledTimes(1);
+        expect(resSetHeader).toHaveBeenCalledTimes(1);
+        expect(resSetHeader).toHaveBeenCalledWith(
+          'Cache-Control',
+          'public, max-age=60, s-maxage=300, stale-while-revalidate=604800'
+        );
+        expect(resStatus).toHaveBeenCalledTimes(1);
+        expect(resStatus).toHaveBeenCalledWith(200);
+        expect(resJson).toHaveBeenCalledTimes(1);
+        expect(resJson).toHaveBeenCalledWith({
+          offset: '',
+          members: [],
+        });
+      });
+    });
+  });
+
+  describe('and AirtableService fails to retrieve the members', () => {
+    beforeAll(() => {
+      (fetch as jest.Mock).mockImplementationOnce(() => Promise.reject());
+    });
+
+    it('should respond with an error', () => {
+      expect(resStatus).toHaveBeenCalledTimes(1);
+      expect(resStatus).toHaveBeenCalledWith(500);
+      expect(resJson).toHaveBeenCalledTimes(1);
+      expect(resJson).toHaveBeenCalledWith({
+        msg: 'Ups, something went wrong 😕',
+      });
+    });
+  });
+});
