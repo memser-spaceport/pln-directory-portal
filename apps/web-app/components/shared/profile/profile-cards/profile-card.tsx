c46c15757e99bfad09992f178de8b6f42b5719eb
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-08-04 17:42:41 +0100

    feat: make profile card generic

---
+++ apps/web-app/components/shared/profile/profile-cards/profile-card.tsx
@@ -0,0 +1,79 @@
+import { ChevronRightIcon } from '@heroicons/react/solid';
+import { AnchorLink } from '@protocol-labs-network/ui';
+import Image from 'next/image';
+import { TeamLeadBadge } from '../../../shared/members/team-lead-badge/team-lead-badge';
+import { TagsGroup } from '../../../shared/tags-group/tags-group';
+
+interface ProfileCardProps {
+  url: string;
+  isImageRounded?: boolean;
+  imageUrl?: string;
+  avatarIcon: (props: React.ComponentProps<'svg'>) => JSX.Element;
+  name: string;
+  showTeamLeadBadge?: boolean;
+  description?: string;
+  tags?: string[];
+}
+
+export function ProfileCard({
+  url,
+  isImageRounded,
+  imageUrl,
+  avatarIcon,
+  name,
+  showTeamLeadBadge,
+  description,
+  tags,
+}: ProfileCardProps) {
+  const Icon = avatarIcon;
+
+  return (
+    <div className="group h-[72px] w-full border-b border-slate-200 bg-white transition-all first:rounded-t-xl last:rounded-b-xl last:border-b-0 hover:bg-slate-50">
+      <AnchorLink href={url}>
+        <div className="flex items-center p-4">
+          <div className="relative mr-4 h-10 w-10 shrink-0">
+            <div
+              className={`flex h-10 w-10 overflow-hidden ${
+                isImageRounded ? 'rounded-full' : 'rounded-lg'
+              }  ${imageUrl ? '' : 'bg-slate-200'}`}
+            >
+              {imageUrl ? (
+                <Image
+                  className={isImageRounded ? 'rounded-full' : 'rounded-lg'}
+                  alt={`${name} img`}
+                  src={imageUrl}
+                  layout="fill"
+                  objectFit={isImageRounded ? 'cover' : 'contain'}
+                  objectPosition="center"
+                />
+              ) : (
+                <Icon className="mt-1 h-11 w-11 fill-white" />
+              )}
+            </div>
+            {showTeamLeadBadge ? (
+              <div className="absolute -top-1 -right-1 z-10">
+                <TeamLeadBadge />
+              </div>
+            ) : null}
+          </div>
+          <div className="mr-4 w-60">
+            <h3 className="text-sm font-semibold">{name}</h3>
+            <p className="text-xs leading-[14px] text-slate-600">
+              {description}
+            </p>
+          </div>
+          <div>
+            {tags?.length ? (
+              <TagsGroup items={tags} isSingleLine={true} />
+            ) : (
+              '-'
+            )}
+          </div>
+          <div className="ml-auto w-12">
+            <ChevronRightIcon className="h-4 w-4 fill-slate-500 group-hover:fill-slate-900" />
+          </div>
+        </div>
+      </AnchorLink>
+    </div>
+  );
+}
