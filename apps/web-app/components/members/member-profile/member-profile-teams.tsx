776198ef33197aab5ca3b7a16163adcecbd699c1
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-08-05 16:13:40 +0100

    feat: update member profile teams section

--- apps/web-app/components/members/member-profile/member-profile-teams.tsx
+++ apps/web-app/components/members/member-profile/member-profile-teams.tsx
@@ -1,19 +1,31 @@
+import { UserGroupIcon } from '@heroicons/react/solid';
 import { ITeam } from '@protocol-labs-network/api';
-import { TeamCard } from '../../shared/teams/team-card/team-card';
+import { useRouter } from 'next/router';
+import { ProfileCard } from '../../shared/profile/profile-cards/profile-card';
+import { ProfileCards } from '../../shared/profile/profile-cards/profile-cards';
 
 interface MemberProfileTeamsProps {
   teams: ITeam[];
 }
 
 export function MemberProfileTeams({ teams }: MemberProfileTeamsProps) {
+  const {
+    query: { id },
+  } = useRouter();
+
   return (
-    <section>
-      <h3 className="mb-2 font-medium text-slate-400">Teams</h3>
-      <div className="flex flex-wrap gap-5">
-        {teams.map((team) => {
-          return <TeamCard key={team.id} team={team} />;
-        })}
-      </div>
-    </section>
+    <ProfileCards title="Teams" count={teams.length}>
+      {teams.map((team) => (
+        <ProfileCard
+          key={`${id}.${team.id}`}
+          url={`/teams/${team.id}`}
+          imageUrl={team.logo}
+          avatarIcon={UserGroupIcon}
+          name={team.name}
+          description={team.shortDescription || team.longDescription}
+          tags={team.tags}
+        />
+      ))}
+    </ProfileCards>
   );
 }
