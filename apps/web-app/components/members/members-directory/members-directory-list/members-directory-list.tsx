ac9dbbec21140044ccd618930e5feaeb8702524f
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-08-08 12:48:27 +0100

    feat: add members directory list component

---
+++ apps/web-app/components/members/members-directory/members-directory-list/members-directory-list.tsx
@@ -0,0 +1,31 @@
+import { IMember } from '@protocol-labs-network/api';
+import { MemberCard } from '../../../../components/shared/members/member-card/member-card';
+import { DirectoryList } from '../../../directory/directory-list/directory-list';
+
+interface MembersDirectoryListProps {
+  members: IMember[];
+  isGrid: boolean;
+}
+
+export function MembersDirectoryList({
+  members,
+  isGrid,
+}: MembersDirectoryListProps) {
+  return (
+    <DirectoryList
+      filterProperties={[
+        'skills',
+        'country',
+        'metroArea',
+        'officeHoursOnly',
+        'includeFriends',
+        'searchBy',
+      ]}
+      itemsCount={members.length}
+    >
+      {members.map((member) => (
+        <MemberCard key={member.id} member={member} isGrid={isGrid} />
+      ))}
+    </DirectoryList>
+  );
+}
