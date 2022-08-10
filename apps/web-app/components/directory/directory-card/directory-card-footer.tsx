6b5ce5d221df0e2ac62ca69019be614721311aae
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-08-10 21:21:23 +0100

    fix: use reducer instead of multiple states on hidden tags hook

--- apps/web-app/components/directory/directory-card/directory-card-footer.tsx
+++ apps/web-app/components/directory/directory-card/directory-card-footer.tsx
@@ -11,7 +11,7 @@ export function DirectoryCardFooter({
   return (
     <>
       {isGrid ? <div className="my-4 h-px bg-slate-200"></div> : null}
-      <div className={isGrid ? 'min-h-[26px]' : 'w-[373px] self-center'}>
+      <div className={isGrid ? 'min-h-[26px]' : 'w-[360px] self-center'}>
         {tagsArr?.length ? <TagsGroup isSingleLine items={tagsArr} /> : '-'}
       </div>
     </>
