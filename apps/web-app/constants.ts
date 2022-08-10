fae89f4bb83b419b4d4d02e34dbc524c712c389c
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-08-10 16:42:40 +0100

    feat: track active directory filters on fathom

--- apps/web-app/constants.ts
+++ apps/web-app/constants.ts
@@ -1,2 +1,37 @@
 export const URL_QUERY_VALUE_SEPARATOR = '|';
 export const ITEMS_PER_PAGE = 9;
+export const FATHOM_EVENTS = {
+  members: {
+    directory: {
+      filters: {
+        officeHoursOnly: 'FQZFKGB0',
+        includeFriends: '1LNSORWL',
+        skills: 'OB2SCUQK',
+        region: '9DFPHYUF',
+        country: 'EMEZBS7G',
+        metroArea: 'O1I7TF4M',
+      },
+      controls: {
+        searchBy: '8BWPXHVB',
+        sort: 'WOZNMS0E',
+        viewType: 'ZZYU8VJV',
+      },
+    },
+  },
+  teams: {
+    directory: {
+      filters: {
+        includeFriends: '2SQSL0FQ',
+        tags: 'BSOPLO3Z',
+        acceleratorPrograms: 'LWE6VIGX',
+        fundingStage: 'L4SN5SWW',
+        technology: 'QSLCUGEX',
+      },
+      controls: {
+        searchBy: 'E5KGF8SF',
+        sort: 'R0U6VIQR',
+        viewType: '9E11MZ5Q',
+      },
+    },
+  },
+};
