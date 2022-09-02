68774918f4fc2bf53d803e6339fbb1f6088ff170
Pedro Fonseca <pedrofonseca@wearepixelmatters.com>
2022-09-02 12:12:01 +0100

    feat(pl-196): infrastructure Scaffolding

--- apps/web-api/src/main.ts
+++ apps/web-api/src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as Sentry from '@sentry/node';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+  });
   await app.listen(3000);
 }
 bootstrap();
