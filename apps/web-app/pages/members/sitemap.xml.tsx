a364a296be2b20473683e9f1e4e50a869b78b66d
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-09-19 16:08:34 +0100

    feat: include teams and members profiles in the sitemap

---
+++ apps/web-app/pages/members/sitemap.xml.tsx
@@ -0,0 +1,23 @@
+import airtableService from '@protocol-labs-network/airtable';
+import { GetServerSideProps } from 'next';
+import { getServerSideSitemap, ISitemapField } from 'next-sitemap';
+import { getMembersDirectoryRequestOptionsFromQuery } from '../../utils/api/list.utils';
+import { getSiteUrl } from '../../utils/sitemap/sitemap.utils';
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+export default function MembersSitemap() {}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const siteUrl = getSiteUrl(process.env.VERCEL_ENV, process.env.VERCEL_URL);
+  const membersListOptions = getMembersDirectoryRequestOptionsFromQuery({
+    includeFriends: 'true',
+  });
+  const members = await airtableService.getMembers(membersListOptions);
+
+  const membersProfiles: ISitemapField[] = members.map((member) => ({
+    loc: `${siteUrl}/members/${member.id}`,
+    lastmod: new Date().toISOString(),
+  }));
+
+  return getServerSideSitemap(context, membersProfiles);
+};
---
+++ apps/web-app/tests/utils/sitemap/sitemap.utils.spec.ts
@@ -0,0 +1,17 @@
+import { getSiteUrl } from '../../../utils/sitemap/sitemap.utils';
+
+describe('#getSiteUrl', () => {
+  const PREVIEW_URL = 'preview-url.com';
+
+  it('should return the production URL when environment is production', () => {
+    expect(getSiteUrl('production')).toEqual('https://plnetwork.io');
+  });
+
+  it('should return the preview URL when environment is not production and the preview URL is defined', () => {
+    expect(getSiteUrl(null, PREVIEW_URL)).toEqual(`https://${PREVIEW_URL}`);
+  });
+
+  it('should return the localhost URL when environment is not production and the preview URL is not defined', () => {
+    expect(getSiteUrl(null, null)).toEqual('http://localhost:4200');
+  });
+});
