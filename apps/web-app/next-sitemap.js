dd596c5ceaa47c35377768c3029f77d2b59ce8d6
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-09-20 16:47:13 +0100

    fix: update next-sitemap configuration

--- apps/web-app/next-sitemap.js
+++ apps/web-app/next-sitemap.js
@@ -1,9 +1,18 @@
 const { getSiteUrl } = require('../web-app/utils/sitemap/sitemap.utils');
 
+const siteUrl = getSiteUrl(process.env.VERCEL_ENV, process.env.VERCEL_URL);
+
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: getSiteUrl(process.env.VERCEL_ENV, process.env.VERCEL_URL),
+  siteUrl,
   generateRobotsTxt: process.env.VERCEL_ENV === 'production',
   sourceDir: 'dist/apps/web-app/.next',
   outDir: 'dist/apps/web-app/public',
+  exclude: ['/members/sitemap.xml', '/teams/sitemap.xml'],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      `${siteUrl}/members/sitemap.xml`,
+      `${siteUrl}/teams/sitemap.xml`,
+    ],
+  },
 };
