f59c73ad77911fa86b2adba445540717de0fca0a
Pedro Fonseca <pedrofonseca@wearepixelmatters.com>
2022-09-22 12:36:45 +0100

    fix(pl-196): add comments to sentry interceptor

--- apps/web-api/src/interceptor/sentry.interceptor.ts
+++ apps/web-api/src/interceptor/sentry.interceptor.ts
@@ -9,10 +9,15 @@ import { catchError, Observable } from 'rxjs';
 
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
+  /**
+   * It intercepts the request, checks if the environment is production, if it is, it sends the error
+   * to Sentry
+   */
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     if (process.env.environment !== 'production') {
       return;
     }
+    /* Catching the error and sending it to Sentry. */
     return next.handle().pipe(
       catchError((exception) => {
         Sentry.captureException(exception);
