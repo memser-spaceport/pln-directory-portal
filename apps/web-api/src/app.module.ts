9f6e10594011d2c0792d7a28692f221f9a2c45ab
Pedro Fonseca <pedrofonseca@wearepixelmatters.com>
2022-09-22 14:55:47 +0100

    feat(pl-243): add api throttling

--- apps/web-api/src/app.module.ts
+++ apps/web-api/src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { HealthModule } from './health/health.module';
 import { MembersModule } from './members/members.module';
 import { PrismaService } from './prisma.service';
 import { TeamsModule } from './teams/teams.module';
-
 @Module({
-  imports: [MembersModule, HealthModule, TeamsModule],
-  providers: [PrismaService],
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 1,
+      limit: 5,
+    }),
+    MembersModule,
+    HealthModule,
+    TeamsModule,
+  ],
+  providers: [
+    PrismaService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
