8b987d42394955416f20f9942512a618ab34d320
Pedro Fonseca <pedrofonseca@wearepixelmatters.com>
2022-09-22 17:29:17 +0100

    feat(pl-245): add cors configuration

--- apps/web-api/src/main.ts
+++ apps/web-api/src/main.ts
@@ -3,6 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
+const origin = {
+  development: /localhost/,
+  staging: /.-protocol-labs-spaceport.vercel.app/,
+  production: 'https://www.plnetwork.io',
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -20,6 +26,11 @@ async function bootstrap() {
     type: VersioningType.URI,
   });
 
+  app.enableCors({
+    origin: origin[process.env.ENVIRONMENT],
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
