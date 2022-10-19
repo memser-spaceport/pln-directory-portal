9b94586da5c6ddc19e2cbe60cf3145aeb7aeaa34
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-10-19 18:22:05 +0100

    fix(PL-327): prevent programmatic navigation to network portal page

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -1,3 +1,16 @@
+import { GetServerSideProps } from 'next';
+
 export default function Index() {
   return <h1>Network Portal</h1>;
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return process.env.NEXT_PUBLIC_HIDE_NETWORK_PORTAL
+    ? {
+        redirect: {
+          permanent: false,
+          destination: '/directory/teams',
+        },
+      }
+    : { props: {} };
+};
