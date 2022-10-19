5a6cae86d5d89e1cac35ed67598e7147d11c4e37
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-10-19 19:22:54 +0100

    feat(PL-272): move portal navbar into portal header folder

--- apps/web-app/components/portal/portal-header/portal-navbar/portal-navbar.tsx
+++ apps/web-app/components/portal/portal-header/portal-navbar/portal-navbar.tsx
@@ -1,4 +1,4 @@
-import { PORTAL_NAVBAR_ITEMS } from './portal-navbar.constants';
+import { PORTAL_HEADER_LINKS } from '../portal-header.constants';
 
 interface PortalNavbarProps {
   floating: boolean;
@@ -11,11 +11,11 @@ export function PortalNavbar({ floating }: PortalNavbarProps) {
         floating
           ? 'bg-white/[.92] shadow-[0_1px_4px_2px_rgba(15,23,42,.04)]'
           : 'bg-white shadow-[0_2px_4px_2px_rgba(15,23,42,.04)]'
-      } p-2 `}
+      } p-2 transition-all duration-700 ease-in-out`}
     >
-      {PORTAL_NAVBAR_ITEMS.map((item) => (
+      {PORTAL_HEADER_LINKS.map((item) => (
         <a
-          className="rounded bg-white py-1 px-2 outline-none outline-1 outline-offset-0 outline-white ring-2 ring-white ring-opacity-25 ring-offset-1 transition-all hover:bg-blue-600 hover:text-white focus:outline-blue-600 focus:ring-blue-600 focus:ring-opacity-25"
+          className="rounded py-1 px-2 outline-none transition-all duration-300 ease-in-out hover:bg-blue-600 hover:text-white focus:outline-1 focus:outline-offset-0 focus:outline-blue-600 focus:ring-2 focus:ring-blue-600 focus:ring-opacity-25 focus:ring-offset-1"
           href={item.url}
           key={item.label}
           target="_blank"
