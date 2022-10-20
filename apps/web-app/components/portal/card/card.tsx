6d5fbd4203a4e64dbaeaf5424c1dbf388bc75eb7
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-10-20 13:24:53 +0100

    feat(PL-323): improve card shadows

--- apps/web-app/components/portal/card/card.tsx
+++ apps/web-app/components/portal/card/card.tsx
@@ -6,7 +6,7 @@ export interface CardProps {
 export function Card({ styleClassName, children }: CardProps) {
   return (
     <div
-      className={`rounded-lg border border-slate-300 p-8 shadow-slate-900/5 ${styleClassName}`}
+      className={`rounded-lg border border-slate-300 p-8 shadow-[0_4px_4px_0px_rgba(15,23,42,.02)] ${styleClassName}`}
     >
       {children}
     </div>
