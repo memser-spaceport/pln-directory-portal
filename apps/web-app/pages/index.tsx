24b34021731def1c6012226c375b45c87a7a1356
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-10-20 13:25:23 +0100

    feat(PL-323): assemble network portal page

--- apps/web-app/pages/index.tsx
+++ apps/web-app/pages/index.tsx
@@ -1,6 +1,14 @@
 import { GetServerSideProps } from 'next';
 import { NextSeo } from 'next-seo';
 import { ReactElement } from 'react';
+import { PortalDivider } from '../components/portal/portal-divider/portal-divider';
+import { Directory } from '../components/portal/sections/directory/directory';
+import { Faq } from '../components/portal/sections/faq/faq';
+import { Footer } from '../components/portal/sections/footer/footer';
+import { LabWeek } from '../components/portal/sections/labweek/labweek';
+import { Mission } from '../components/portal/sections/mission/mission';
+import { Projects } from '../components/portal/sections/projects/projects';
+import { Substack } from '../components/portal/sections/substack/substack';
 import { PortalLayout } from '../layouts/portal-layout';
 
 export default function Index() {
@@ -11,6 +19,36 @@ export default function Index() {
           { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         ]}
       />
+
+      <div>
+        <Mission />
+        <PortalDivider />
+        <div className="bg-white py-24 px-6 sm:px-16 sm:py-[120px]">
+          <div className="mx-auto max-w-[1110px]">
+            <Directory />
+          </div>
+        </div>
+        <div className="bg-gradient-to-b from-slate-50 to-white">
+          <div className="py-24 px-6 sm:px-16 sm:py-[152px]">
+            <div className="mx-auto max-w-[1110px]">
+              <div className="mb-[106px] sm:mb-[162px]">
+                <LabWeek />
+              </div>
+              <div className="mb-[72px] sm:mb-40">
+                <Projects />
+              </div>
+              <div className="mb-32 sm:mb-48">
+                <Substack />
+              </div>
+              <div className="mx-auto max-w-[800px]">
+                <Faq />
+              </div>
+            </div>
+          </div>
+          <PortalDivider />
+          <Footer />
+        </div>
+      </div>
     </>
   );
 }
