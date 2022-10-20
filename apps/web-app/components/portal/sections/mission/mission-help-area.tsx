39b098a3fdece647618aa045768f542c9caa912c
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-10-20 17:08:26 +0100

    fix: update mission section's help area text alignment

--- apps/web-app/components/portal/sections/mission/mission-help-area.tsx
+++ apps/web-app/components/portal/sections/mission/mission-help-area.tsx
@@ -10,7 +10,7 @@ export const MissionHelpArea = ({
   areaName,
 }: TMissionHelpAreaProps) => {
   return (
-    <div className="flex items-center gap-x-3">
+    <div className="flex items-center gap-x-3 text-left">
       <div className="flex h-6 w-6 items-center justify-center rounded-full bg-black/[0.04]">
         {areaIcon}
       </div>
