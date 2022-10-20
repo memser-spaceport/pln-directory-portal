60bedf891d8d617a730f49017fb04d0eb39d5988
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-10-20 17:18:18 +0100

    feat(PL-339): remove faq item

--- apps/web-app/components/portal/sections/faq/faq.tsx
+++ apps/web-app/components/portal/sections/faq/faq.tsx
@@ -16,11 +16,6 @@ const faqItems = [
     content:
       "<p>We welcome teams interested in the Protocol Labs mission to collaborate with us on a short or long-term basis. You can read more about membership options <a href='https://protocol.almanac.io/handbook/protocol-labs-spaceport-JzKymu/pln-membership-G4pIdsLCQ6q0BJwwDTOfaVSeoV3NNd8V' target='_blank' rel='noopener noreferrer'><u>here</u></a>, or reach out to <a href='mailto:spaceport-admin@protocol.ai' title='mailto:spaceport-admin@protocol.ai'><u>spaceport-admin@protocol.ai</u></a> and we'll take things from there.</p>",
   },
-  {
-    triggerText: 'What is Spaceport?',
-    content:
-      '<p>Spaceport is your one-stop shop for Protocol Labs Network resources and member benefits. <br/>Starting and scaling a web3 project is hard, and it can be lonely working in a distributed and remote environment. We’d like to make this easier. The best way to do this, in our experience, is to help individuals and teams connect with and learn from each other. Spaceport was created to make these connections as low friction as possible–through office hours, events, a network directory, and more–so that <strong>together</strong> we can build the protocols and technology that advances humanity.</p>',
-  },
 ];
 
 export const Faq = () => {
