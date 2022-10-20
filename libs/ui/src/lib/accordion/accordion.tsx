53e6b0c7492e6134dea5158d280d000dc8ff3fa4
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-10-20 13:24:33 +0100

    feat(PL-323): improve accordion transitions

--- libs/ui/src/lib/accordion/accordion.tsx
+++ libs/ui/src/lib/accordion/accordion.tsx
@@ -12,9 +12,9 @@ export function Accordion({ items }: AccordionProps) {
         return (
           <Disclosure key={i}>
             {({ open }) => (
-              <div className="rounded-lg border border-slate-200 bg-white p-8">
-                <Disclosure.Button className="flex w-full items-start text-left">
-                  <span className="w-full text-2xl font-semibold">
+              <div className="rounded-lg border border-slate-200 bg-white">
+                <Disclosure.Button className="flex w-full items-start p-8 text-left">
+                  <span className="w-full pr-8 text-2xl font-semibold">
                     {triggerText}
                   </span>
                   <span
@@ -26,14 +26,19 @@ export function Accordion({ items }: AccordionProps) {
                   </span>
                 </Disclosure.Button>
                 <Transition
-                  enter="transition ease-in-out duration-300 transform"
-                  enterFrom="-translate-y-2/4 opacity-0"
-                  enterTo="translate-y-0 opacity-100"
-                  leave="transition ease-in-out duration-300 transform"
-                  leaveFrom="opacity-100"
-                  leaveTo="opacity-0"
+                  show={open}
+                  className="overflow-hidden"
+                  enter="transition-[max-height] duration-300 ease-in-out"
+                  enterFrom="transform max-h-0"
+                  enterTo="transform max-h-screen"
+                  leave="transition-[max-height] duration-300 ease-in-out"
+                  leaveFrom="transform max-h-screen"
+                  leaveTo="transform max-h-0"
                 >
-                  <Disclosure.Panel className="mt-4 text-lg text-slate-600">
+                  <Disclosure.Panel
+                    className="p-8 pt-0 text-lg text-slate-600"
+                    static
+                  >
                     <div dangerouslySetInnerHTML={{ __html: content }} />
                   </Disclosure.Panel>
                 </Transition>
