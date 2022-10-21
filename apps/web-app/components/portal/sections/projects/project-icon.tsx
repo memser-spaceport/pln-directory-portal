00d527d11c6e3e5fe66cb6efeb0b385304cd0d54
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-10-21 12:53:44 +0100

    feat(PL-342): add alt text to portal projects' logos

--- apps/web-app/components/portal/sections/projects/project-icon.tsx
+++ apps/web-app/components/portal/sections/projects/project-icon.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image';
 
-export const ProjectIcon = ({ imageFile }) => (
+type ProjectIconProps = {
+  alt: string;
+  imageFile: string;
+};
+
+export const ProjectIcon = ({ alt, imageFile }: ProjectIconProps) => (
   <div className="flex h-12 w-12 items-center justify-center rounded-full bg-black/[0.04]">
     <div className="relative h-8 w-8">
       <Image
@@ -8,6 +13,7 @@ export const ProjectIcon = ({ imageFile }) => (
         layout="fill"
         objectFit="contain"
         objectPosition="center"
+        alt={alt}
       />
     </div>
   </div>
