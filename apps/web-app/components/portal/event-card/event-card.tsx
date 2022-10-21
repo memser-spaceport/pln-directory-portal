2d672eb01e94584fde3b03518c0a9a356cacb675
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-10-21 09:24:56 +0100

    feat: update event cards topic and link

--- apps/web-app/components/portal/event-card/event-card.tsx
+++ apps/web-app/components/portal/event-card/event-card.tsx
@@ -1,6 +1,7 @@
 import { ImageCard } from '../image-card/image-card';
 
 type EventCardProps = {
+  cardUrl: string;
   imageURL: string;
   topic: string;
   eventTitle: string;
@@ -8,24 +9,32 @@ type EventCardProps = {
 };
 
 export const EventCard = ({
+  cardUrl,
   imageURL,
   topic,
   eventTitle,
   eventDetails,
 }: EventCardProps) => {
   return (
-    <ImageCard imageURL={imageURL}>
-      <div className="absolute bottom-0 left-0 w-full p-5 text-left text-white">
-        <span className="leading-3.5 rounded-3xl bg-blue-600 px-3 py-1.5 text-xs font-medium">
-          {topic}
-        </span>
-        <h3 className="mt-2.5 overflow-hidden text-ellipsis whitespace-nowrap text-2xl font-semibold">
-          {eventTitle}
-        </h3>
-        <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-slate-200">
-          {eventDetails}
-        </p>
-      </div>
-    </ImageCard>
+    <a
+      href={cardUrl}
+      className="relative block h-full w-full cursor-pointer"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <ImageCard imageURL={imageURL}>
+        <div className="absolute bottom-0 left-0 w-full p-5 text-left text-white">
+          <span className="leading-3.5 rounded-3xl bg-blue-600 px-3 py-1.5 text-xs font-medium">
+            {topic}
+          </span>
+          <h3 className="mt-2.5 overflow-hidden text-ellipsis whitespace-nowrap text-2xl font-semibold">
+            {eventTitle}
+          </h3>
+          <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm text-slate-200">
+            {eventDetails}
+          </p>
+        </div>
+      </ImageCard>
+    </a>
   );
 };
