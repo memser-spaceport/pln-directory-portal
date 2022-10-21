7bf75d17bf324d01464bc01cb13d72c3159179d4
Miguel Leite <miguelleite@wearepixelmatters.com>
2022-10-21 15:59:34 +0100

    feat(PL-345): update above the fold items icons background

--- apps/web-app/components/portal/sections/mission/mission-help-area.tsx
+++ apps/web-app/components/portal/sections/mission/mission-help-area.tsx
@@ -11,7 +11,7 @@ export const MissionHelpArea = ({
 }: TMissionHelpAreaProps) => {
   return (
     <div className="flex items-center gap-x-3 text-left">
-      <div className="flex h-6 w-6 items-center justify-center rounded-full bg-black/[0.04]">
+      <div className="flex h-6 w-6 items-center justify-center rounded-full bg-blue-50">
         {areaIcon}
       </div>
       {areaName}
