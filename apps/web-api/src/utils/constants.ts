7f57ea6413b61cb6aed639ef7261067d578cc836
Miguel Teixeira <teixeira.silva.miguel@gmail.com>
2022-11-22 16:02:38 +0000

    fix: require redis tls only for remote environments

--- apps/web-api/src/utils/constants.ts
+++ apps/web-api/src/utils/constants.ts
@@ -9,3 +9,5 @@ export const ALLOWED_CORS_ORIGINS = {
   [APP_ENV.STAGING]: /.-protocol-labs-spaceport.vercel.app/,
   [APP_ENV.PRODUCTION]: 'https://www.plnetwork.io',
 };
+
+export const IS_DEV_ENVIRONMENT = process.env.ENVIRONMENT !== APP_ENV.DEV;
