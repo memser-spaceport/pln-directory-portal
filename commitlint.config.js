a5b1a1e362b8e0ff19d9486ff9cccf06ccc347d9
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-11-28 17:43:34 +0000

    feat(PL-360): enforce commit guidelines

---
+++ commitlint.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  plugins: ['commitlint-plugin-function-rules'],
+  rules: {
+    // Disable generic validation
+    'scope-case': [0],
+    'scope-empty': [0],
+
+    // Custom scope validation
+    'function-rules/scope-case': [
+      2,
+      'always',
+      (commit) => {
+        const hasScope = !!commit.scope;
+        const hasValidScope = hasScope && /^PL-[0-9]+$/.test(commit.scope);
+        const optionalScopeTypes = ['build', 'ci', 'docs', 'revert'];
+        const isScopeOptional = optionalScopeTypes.includes(commit.type);
+
+        return hasValidScope || (isScopeOptional && !hasScope)
+          ? [true]
+          : [
+              false,
+              'scope must be PL-XXX where XXX is a valid JIRA ticket number',
+            ];
+      },
+    ],
+  },
+};
