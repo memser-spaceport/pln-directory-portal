68a960bd4095f7db8eaf99366481f416a5f36723
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-12-05 23:47:56 +0000

    feat(PL-412): create skills data library

---
+++ libs/skills/data-access/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'skills-data-access',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/skills/data-access',
+};
