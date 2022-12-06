86385fad6486c718a45a8fdd3b21f17e34e1290f
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-12-06 11:47:41 +0000

    feat(PL-383): create teams data library

---
+++ libs/teams/data-access/README.md
@@ -0,0 +1,7 @@
+# teams-data-access
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test teams-data-access` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/teams/data-access/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'teams-data-access',
+  preset: '../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../coverage/libs/teams/data-access',
+};
