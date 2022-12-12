ae7c1daaca1e32784cd88f76f00a309d02d38990
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-12-12 17:05:57 +0000

    feat(PL-384): create method for getting all teams

--- libs/shared/data-access/src/index.ts
+++ libs/shared/data-access/src/index.ts
@@ -1,6 +1,7 @@
 import { apiNested } from '@protocol-labs-network/contracts';
 import { initClient } from '@ts-rest/core';
 import { env } from 'process';
+export * from './shared.types';
 
 export const client = initClient(apiNested, {
   baseUrl: env['WEB_API_BASE_URL'] as string,
