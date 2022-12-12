ebee16b7972e4caaf210a15176e9c0d8bce30d07
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-12-12 22:32:01 +0000

    feat(PL-421): rename lib name from technology to technologies for consistency

--- libs/technologies/data-access/jest.config.js
+++ libs/technologies/data-access/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'technology-data-access',
+  displayName: 'technologies-data-access',
   preset: '../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/technology/data-access',
+  coverageDirectory: '../../../coverage/libs/technologies/data-access',
 };
