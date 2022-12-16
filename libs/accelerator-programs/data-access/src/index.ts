77f712a64cf8032035334796c9b5e05ce7d34345
AnaFigueiredo <29309910+AnaFigueiredo26@users.noreply.github.com>
2022-12-16 12:40:35 +0000

    feat(PL-388): make data-access methods returns uniform

--- libs/accelerator-programs/data-access/src/index.ts
+++ libs/accelerator-programs/data-access/src/index.ts
@@ -1,10 +1,5 @@
 import { client } from '@protocol-labs-network/shared/data-access';
 
 export const getAcceleratorPrograms = async () => {
-  const { body, status } =
-    await client.acceleratorPrograms.getAcceleratorPrograms({
-      query: {},
-    });
-
-  return { body, status };
+  return await client.acceleratorPrograms.getAcceleratorPrograms();
 };
