5995de282948729fcf2abad94e2abb090688ea8d
Pedro Fonseca <pedrofonseca@wearepixelmatters.com>
2023-01-03 10:33:10 +0000

    feat(PL-438): add file migration scripts

--- apps/web-api/src/teams/teams.module.ts
+++ apps/web-api/src/teams/teams.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ImagesController } from '../images/images.controller';
+import { ImagesService } from '../images/images.service';
 import { PrismaService } from '../prisma.service';
+import { FileEncryptionService } from '../utils/file-encryption/file-encryption.service';
+import { FileMigrationService } from '../utils/file-migration/file-migration.service';
+import { FileUploadService } from '../utils/file-upload/file-upload.service';
 import { TeamsController } from './teams.controller';
 import { TeamsService } from './teams.service';
 
 @Module({
   controllers: [TeamsController],
-  providers: [TeamsService, PrismaService],
+  providers: [
+    TeamsService,
+    PrismaService,
+    FileMigrationService,
+    ImagesController,
+    ImagesService,
+    FileUploadService,
+    FileEncryptionService,
+  ],
 })
 export class TeamsModule {}
---
+++ apps/web-api/src/utils/file-migration/file-migration.service.spec.ts
@@ -0,0 +1,157 @@
+import { createMock } from '@golevelup/ts-jest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { prisma } from 'apps/web-api/prisma/__mocks__';
+import fs from 'fs';
+import { ImagesController } from '../../images/images.controller';
+import { ImagesService } from '../../images/images.service';
+import { PrismaService } from '../../prisma.service';
+import { FileEncryptionService } from '../file-encryption/file-encryption.service';
+import { FileUploadService } from '../file-upload/file-upload.service';
+import { FileMigrationService } from './file-migration.service';
+
+jest.spyOn(fs, 'readFileSync').mockImplementation(() => 'readFileSync');
+jest.spyOn(fs, 'createWriteStream').mockImplementation();
+jest.spyOn(fs, 'unlink').mockImplementation();
+jest.spyOn(fs, 'createReadStream').mockImplementation();
+
+describe('FileMigrationService', () => {
+  let fileMigrationService: FileMigrationService;
+  let imagesController: ImagesController;
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [
+        FileMigrationService,
+        PrismaService,
+        {
+          provide: FileUploadService,
+          useValue: createMock<FileUploadService>(),
+        },
+        {
+          provide: FileEncryptionService,
+          useValue: createMock<FileEncryptionService>(),
+        },
+        { provide: ImagesController, useValue: createMock<ImagesController>() },
+        { provide: ImagesService, useValue: createMock<ImagesService>() },
+      ],
+    })
+      .overrideProvider(PrismaService)
+      .useValue(prisma)
+      .compile();
+
+    fileMigrationService = app.get<FileMigrationService>(FileMigrationService);
+    imagesController = app.get<ImagesController>(ImagesController);
+    jest.spyOn(imagesController, 'uploadImage').mockImplementation(() => {
+      return Promise.resolve({
+        image: {
+          cid: 'cid',
+          filename: 'filename',
+          size: 1,
+          type: 'type',
+          width: 1,
+          height: 1,
+          url: 'url',
+          uid: 'uid',
+          version: 'LARGE',
+          id: 1,
+          createdAt: new Date(),
+          thumbnails: [],
+          thumbnailToUid: null,
+          updatedAt: new Date(),
+        },
+      });
+    });
+
+    jest.spyOn(fileMigrationService, 'download').mockImplementation(() => {
+      return Promise.resolve();
+    });
+  });
+
+  describe('When receiving a file', () => {
+    it('Should call uploadImage with a valid image', async () => {
+      const airtableImage = {
+        id: 'rec1',
+        url: 'https://dl.airtable.com/.attachments/test.png',
+        filename: '1',
+        size: 1,
+        type: 'image/png',
+        width: 1,
+        height: 1,
+      };
+      await fileMigrationService.migrateFile(airtableImage);
+      expect(imagesController.uploadImage).toHaveBeenCalledWith({
+        filename: '1',
+        mimetype: 'image/png',
+        encoding: '7bit',
+        buffer: 'readFileSync',
+        destination: '',
+        fieldname: 'file',
+        originalname: '1',
+        path: './1',
+        size: 1,
+        stream: undefined,
+      });
+    });
+    it('Should return status if file type is not suported', async () => {
+      const airtableImage = {
+        id: 'rec1',
+        url: 'https://dl.airtable.com/.attachments/test.png',
+        filename: '1',
+        size: 1,
+        type: 'image/unsupported',
+        width: 1,
+        height: 1,
+      };
+      const result = await fileMigrationService.migrateFile(airtableImage);
+      expect(result).toEqual({ status: 'File type not supported' });
+    });
+    it('Should return status if the size is bigger than 1mb', async () => {
+      const airtableImage = {
+        id: 'rec1',
+        url: 'https://dl.airtable.com/.attachments/test.png',
+        filename: '1',
+        size: 100000000,
+        type: 'image/png',
+        width: 1,
+        height: 1,
+      };
+      const result = await fileMigrationService.migrateFile(airtableImage);
+      expect(result).toEqual({ status: 'File size too large' });
+    });
+    it('Should throw error if the download fails', async () => {
+      const airtableImage = {
+        id: 'rec1',
+        url: 'https://dl.airtable.com/.attachments/test.png',
+        filename: '1',
+        size: 1,
+        type: 'image/png',
+        width: 1,
+        height: 1,
+      };
+      jest
+        .spyOn(fileMigrationService, 'download')
+        .mockImplementation((uri, filename) => {
+          return Promise.reject();
+        });
+      await expect(
+        fileMigrationService.migrateFile(airtableImage)
+      ).rejects.toThrow();
+    });
+    it('Should throw error if the upload fails', async () => {
+      const airtableImage = {
+        id: 'rec1',
+        url: 'https://dl.airtable.com/.attachments/test.png',
+        filename: '1',
+        size: 1,
+        type: 'image/png',
+        width: 1,
+        height: 1,
+      };
+      jest.spyOn(imagesController, 'uploadImage').mockImplementation(() => {
+        return Promise.reject();
+      });
+      await expect(
+        fileMigrationService.migrateFile(airtableImage)
+      ).rejects.toThrow();
+    });
+  });
+});
