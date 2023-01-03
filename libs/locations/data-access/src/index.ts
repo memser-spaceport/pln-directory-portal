2e9a0ab76517b6779ba34ec67397353f6e1c96eb
Miguel Leite <miguelleite@pixelmatters.com>
2023-01-03 16:06:29 +0000

    feat(PL-389): add the ability to fetch all location details at once

--- libs/locations/data-access/src/index.ts
+++ libs/locations/data-access/src/index.ts
@@ -1,34 +1,37 @@
 import { client } from '@protocol-labs-network/shared/data-access';
 
 /**
+ * Get locations
+ */
+export const getLocations = async () => {
+  return await client.locations.getLocations({
+    query: { select: 'city,region,country' },
+  });
+};
+
+/**
  * Get distinct values for existing cities within the locations
  */
-export const getMetroAreas = async () => {
-  const { body, status } = await client.locations.getLocations({
+export const getCities = async () => {
+  return await client.locations.getLocations({
     query: { select: 'city', distinct: 'city' },
   });
-
-  return { body, status };
 };
 
 /**
  * Get distinct values for existing countries within the locations
  */
 export const getCountries = async () => {
-  const { body, status } = await client.locations.getLocations({
+  return await client.locations.getLocations({
     query: { select: 'country', distinct: 'country' },
   });
-
-  return { body, status };
 };
 
 /**
  * Get distinct values for existing regions within the locations
  */
 export const getRegions = async () => {
-  const { body, status } = await client.locations.getLocations({
+  return await client.locations.getLocations({
     query: { select: 'region', distinct: 'region' },
   });
-
-  return { body, status };
 };
