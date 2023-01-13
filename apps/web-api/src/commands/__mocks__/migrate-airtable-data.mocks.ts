7a2d8daf6c5026c1f843c10c4fafbad993052e31
Miguel Teixeira <teixeira.silva.miguel@gmail.com>
2023-01-13 18:09:31 +0000

    chore(PL-479): update forest admin schema

--- apps/web-api/src/commands/__mocks__/migrate-airtable-data.mocks.ts
+++ apps/web-api/src/commands/__mocks__/migrate-airtable-data.mocks.ts
@@ -3,7 +3,6 @@ import { FundingStagesService } from '../../funding-stages/funding-stages.servic
 import { IndustryCategoriesService } from '../../industry-categories/industry-categories.service';
 import { IndustryTagsService } from '../../industry-tags/industry-tags.service';
 import { MembersService } from '../../members/members.service';
-import { RolesService } from '../../roles/roles.service';
 import { SkillsService } from '../../skills/skills.service';
 import { TeamMemberRolesService } from '../../team-member-roles/team-member-roles.service';
 import { TeamsService } from '../../teams/teams.service';
@@ -13,7 +12,7 @@ import { AirtableService } from '../../utils/airtable/airtable.service';
 export const TEST_SERVICES_MOCK = (customMethods = {}) =>
   [
     // Build for all services injected the needed mocked methods:
-    ...Array(11)
+    ...Array(10)
       .fill('')
       .map(
         () =>
@@ -31,7 +30,6 @@ export const TEST_SERVICES_MOCK = (customMethods = {}) =>
       ),
   ] as [
     TeamsService,
-    RolesService,
     SkillsService,
     MembersService,
     AirtableService,
