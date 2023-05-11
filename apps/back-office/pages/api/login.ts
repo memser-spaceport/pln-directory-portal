c08df87351ce54cd0bfd3e97d9f7454f4a361386
Aswin <aswin@ideas2it.com>
2023-05-11 13:56:00 +0530

    fix: admin build fix (#831)

--- apps/back-office/pages/api/login.ts
+++ apps/back-office/pages/api/login.ts
@@ -12,7 +12,6 @@ export default async function login(req, res) {
   await api
     .post('/v1/admin/signin', { username: username, password: password })
     .then((response) => {
-      console.log('response>>>>>>', response);
       if (response?.data?.accessToken) {
         // Set the authentication cookie
         const decoded = jwt_decode<DecodedJwtPayload>(
