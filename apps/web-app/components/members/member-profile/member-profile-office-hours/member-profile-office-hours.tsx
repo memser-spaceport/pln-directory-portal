a6817f8557f0cd8c102879ec75ec474ce9ffe782
Thangaraj-Ideas2it <thangaraj.esakky@ideas2it.com>
2023-05-16 19:38:45 +0530

    Fixed code merge issue

--- apps/web-app/components/members/member-profile/member-profile-office-hours/member-profile-office-hours.tsx
+++ apps/web-app/components/members/member-profile/member-profile-office-hours/member-profile-office-hours.tsx
@@ -11,7 +11,7 @@ import useAppAnalytics from 'apps/web-app/hooks/shared/use-app-analytics';
 type MemberProfileOfficeHoursProps = {
   url?: string;
   userInfo: any;
-  member: IMember
+  member?: IMember
 };
 
 const LEARN_MORE_URL =
@@ -33,76 +33,73 @@ export function MemberProfileOfficeHours({
       authenticate();
       trackGoal(loginAsUserCode, 0);
     }
+  };
 
-    const handleOpenModal = () => {
-      authenticate();
-      trackGoal(loginAsUserCode, 0);
-    };
-
-    const onScheduleMeeting = () => {
-      trackGoal(FATHOM_EVENTS.members.profile.officeHours.scheduleMeeting, 0);
+  const onScheduleMeeting = () => {
+    trackGoal(FATHOM_EVENTS.members.profile.officeHours.scheduleMeeting, 0);
+    if(member) {
       analytics.captureEvent('office-hours-clicked', {
         name: member.name,
         uid: member.id
       })
     }
-
-    return (
-      <div className="mt-6 rounded-xl bg-slate-50 p-4">
-        <div className="flex items-center">
-          <span className="mr-3 w-7 rounded bg-blue-100 p-1.5">
-            <CalendarIcon className="stroke-1.5 h-4 w-4 rounded text-blue-700" />
-          </span>
-          <h3 className="text-lg font-semibold">Office Hours</h3>
-        </div>
-        <p className="mt-4 text-base">
-          A 15 minute time slot for meetings that Members can use to discuss some
-          of the most pressing issues their team or organization is currently
-          facing.
-        </p>
-        <div className="mt-6 flex space-x-4">
-          {userInfo.uid ? (
-            <>
-              {url ? (
-                <AnchorLink
-                  href={url}
-                  linkClassName="shadow-request-button rounded-lg border border-slate-300 bg-white px-6 py-2.5 text-sm font-medium hover:shadow-on-hover hover:text-slate-600 on-focus active:border-blue-600 active:ring-2"
-                  handleOnClick={onScheduleMeeting}
-                >
-                  Schedule Meeting
-                </AnchorLink>
-              ) : (
-                <span className="rounded-lg border border-slate-200 bg-white px-6 py-2.5 text-sm font-medium text-slate-400">
-                  Not Available
-                </span>
-              )}
-            </>) :
-            (url ?
-              <>
-                <button
-                  onClick={handleOnClick}
-                  className="shadow-request-button rounded-lg border border-slate-300 bg-white px-6 py-2.5 text-sm font-medium hover:shadow-on-hover hover:text-slate-600 on-focus active:border-blue-600 active:ring-2"
-                >
-                  Login to Schedule
-                </button>
-              </> : <></>
-            )
-          }
-
-          <AnchorLink
-            href={LEARN_MORE_URL}
-            linkClassName="flex items-center text-sm font-semibold group outline-none"
-            handleOnClick={() =>
-              trackGoal(FATHOM_EVENTS.members.profile.officeHours.learnMore, 0)
-            }
-          >
-            <span className="group-focus-within:shadow-[0_1px_0_#156ff7] group-focus:shadow-[0_1px_0_#156ff7] group-focus-visible:shadow-[0_1px_0_#156ff7]">
-              Learn more
-            </span>
-            <ArrowSmRightIcon className="stroke-1.5 ml-1 h-4 w-4 -rotate-45" />
-          </AnchorLink>
-        </div>
-      </div>
-    );
   }
+
+  return (
+    <div className="mt-6 rounded-xl bg-slate-50 p-4">
+      <div className="flex items-center">
+        <span className="mr-3 w-7 rounded bg-blue-100 p-1.5">
+          <CalendarIcon className="stroke-1.5 h-4 w-4 rounded text-blue-700" />
+        </span>
+        <h3 className="text-lg font-semibold">Office Hours</h3>
+      </div>
+      <p className="mt-4 text-base">
+        A 15 minute time slot for meetings that Members can use to discuss some
+        of the most pressing issues their team or organization is currently
+        facing.
+      </p>
+      <div className="mt-6 flex space-x-4">
+        {userInfo.uid ? (
+          <>
+            {url ? (
+              <AnchorLink
+                href={url}
+                linkClassName="shadow-request-button rounded-lg border border-slate-300 bg-white px-6 py-2.5 text-sm font-medium hover:shadow-on-hover hover:text-slate-600 on-focus active:border-blue-600 active:ring-2"
+                handleOnClick={onScheduleMeeting}
+              >
+                Schedule Meeting
+              </AnchorLink>
+            ) : (
+              <span className="rounded-lg border border-slate-200 bg-white px-6 py-2.5 text-sm font-medium text-slate-400">
+                Not Available
+              </span>
+            )}
+          </>) :
+          (url ?
+            <>
+              <button
+                onClick={handleOnClick}
+                className="shadow-request-button rounded-lg border border-slate-300 bg-white px-6 py-2.5 text-sm font-medium hover:shadow-on-hover hover:text-slate-600 on-focus active:border-blue-600 active:ring-2"
+              >
+                Login to Schedule
+              </button>
+            </> : <></>
+          )
+        }
+
+        <AnchorLink
+          href={LEARN_MORE_URL}
+          linkClassName="flex items-center text-sm font-semibold group outline-none"
+          handleOnClick={() =>
+            trackGoal(FATHOM_EVENTS.members.profile.officeHours.learnMore, 0)
+          }
+        >
+          <span className="group-focus-within:shadow-[0_1px_0_#156ff7] group-focus:shadow-[0_1px_0_#156ff7] group-focus-visible:shadow-[0_1px_0_#156ff7]">
+            Learn more
+          </span>
+          <ArrowSmRightIcon className="stroke-1.5 ml-1 h-4 w-4 -rotate-45" />
+        </AnchorLink>
+      </div>
+    </div>
+  );
 }
