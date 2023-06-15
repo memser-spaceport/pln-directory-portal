b7321ffb25255b200eacc32b0d66b82e65982042
Aswin <aswin@ideas2it.com>
2023-06-15 09:56:18 +0530

    fix: class added for style

--- apps/web-app/components/members/member-enrollment/teamandrole.tsx
+++ apps/web-app/components/members/member-enrollment/teamandrole.tsx
@@ -34,7 +34,7 @@ export function TeamAndRoleGrid(props) {
       <div className="flex flex-row">
         <div className="basis-6/12">
           <Autocomplete
-            className="custom-grey custom-outline-none border truncate pr-7"
+            className="custom-grey custom-outline-none border truncate padding-team-autocomplete"
             required={true}
             placeholder="Select a team"
             selectedOption={{ value: team?.teamUid, label: team?.teamTitle }}
