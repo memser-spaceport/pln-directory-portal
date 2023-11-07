2dc4eccb4ff40e81023497a15e07b028558061b8
Thangaraj-Ideas2it <thangaraj.esakky@ideas2it.com>
2023-11-07 15:52:32 +0530

    feat/project-contribution: Support for richtext

---
+++ apps/web-app/components/projects/contribution/project-description.tsx
@@ -0,0 +1,52 @@
+
+import React, {useMemo} from 'react';
+import {InitialConfigType, LexicalComposer} from '@lexical/react/LexicalComposer';
+import {PlainTextPlugin} from "@lexical/react/LexicalPlainTextPlugin";
+import {ContentEditable} from '@lexical/react/LexicalContentEditable';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+
+function ProjectDescription() {
+    const CustomContent = useMemo(() => {
+        return (
+            <ContentEditable style={{
+                position: 'relative',
+                borderColor: 'rgba(255,211,2,0.68)',
+                border: '2px solid red',
+                borderRadius: '5px',
+                maxWidth: '100%',
+                padding: '10px'
+            }}/>
+        )
+    }, []);
+
+    const CustomPlaceholder = useMemo(() => {
+        return (
+            <div style={{
+                position: 'absolute', top: 30, left: 30,
+            }}>
+                Enter Project Contribution...
+            </div>
+        )
+    }, []);
+
+    const lexicalConfig: InitialConfigType = {
+        namespace: 'My Rich Text Editor',
+        onError: (e) => {
+            console.log('ERROR:', e)
+        }
+    }
+
+    return (
+        <div style={{padding: '20px'}}>
+            <LexicalComposer initialConfig={lexicalConfig}>
+                <PlainTextPlugin
+                    contentEditable={CustomContent}
+                    placeholder={CustomPlaceholder}
+                    ErrorBoundary={LexicalErrorBoundary}
+                />
+            </LexicalComposer>
+        </div>
+    );
+}
+
+export default ProjectDescription;
