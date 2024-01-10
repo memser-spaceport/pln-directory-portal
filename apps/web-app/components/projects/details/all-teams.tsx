b2cd2cecadbf1018be6fc8a2438b9b875ff928aa
NivedhaSV <nivedha.selvaraja@ideas2it.com>
2024-01-10 18:30:48 +0530

    fix: high issues reopened

--- apps/web-app/components/projects/details/all-teams.tsx
+++ apps/web-app/components/projects/details/all-teams.tsx
@@ -12,9 +12,6 @@ export function AllTeamsModal({
     setIsModalOpen,
     project,
 }) {
-
-    console.log(project.contributingTeams);
-    
     const router = useRouter();
     const analytics = useAppAnalytics();
 
