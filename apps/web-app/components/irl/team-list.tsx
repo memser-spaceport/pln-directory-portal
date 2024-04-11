8f0c3ebd7e7d035c5aa64fb4bb3f6cab3be7ab2b
Vellaiyan-Marimuthu <vellaiyan.m@ideas2it.com>
2024-04-12 02:19:57 +0530

    feat/irl-new-changes: analytics and new changes implemented

--- apps/web-app/components/irl/team-list.tsx
+++ apps/web-app/components/irl/team-list.tsx
@@ -1,8 +1,28 @@
+import { APP_ANALYTICS_EVENTS } from "apps/web-app/constants";
+import useAppAnalytics from "apps/web-app/hooks/shared/use-app-analytics";
+import { getUserInfo } from "apps/web-app/utils/shared.utils";
 import Link from "next/link";
 
 const TeamList = (props: any) => {
   const items = props?.items ?? [];
-  const onLogin = props.onLogin
+  const onLogin = props.onLogin;
+
+  const analytics = useAppAnalytics();
+  const user = getUserInfo();
+
+  const onLoginClick = ()=> {
+    analytics.captureEvent(APP_ANALYTICS_EVENTS.IRL_GUEST_LIST_TABLE_LOGIN_BTN_CLICKED);
+    onLogin()
+  }
+
+  const onTeamClick = (teamUid:string, teamName:string)=> {
+    analytics.captureEvent(APP_ANALYTICS_EVENTS.IRL_GUEST_LIST_TABLE_TEAM_CLICKED, {
+      teamUid, 
+      teamName,
+      user
+    })
+  }
+
   return <>
     <div className="flex w-[100%] h-fit flex-col pt-[8px] relative">
       {items.map((item, itemIndex) => (
@@ -12,7 +32,7 @@ const TeamList = (props: any) => {
            {item?.teamLogo && <img src={item.teamLogo}   className="bg-gray-200 h-[32px] w-[32px]"/>}
            </div>
              <Link href={`/teams/${item.teamUid}`}>
-             <a target="_blank" className="text-clamp flex-1 break-words">{item.teamName}</a>
+             <a target="_blank" className="text-clamp flex-1 break-words" onClick={()=> onTeamClick(item?.teamUid,item?.teamName)}>{item.teamName}</a>
              </Link>
           </div>
           <div className="hidden lg:flex gap-[4px] w-[180px] items-center justify-start">
@@ -31,7 +51,7 @@ const TeamList = (props: any) => {
       
       <div className="bg-[#C8C8C8AB] hidden lg:block bg-opacity-60 backdrop-blur-[2.5px] absolute top-[4px] right-[1px] bottom-0 w-[calc(100%_-_200px)]">
         <div className="relative w-[100%] h-[100%] flex justify-center">
-        <button onClick={onLogin} className="sticky top-[50%] h-[36px] rounded-[8px] flex items-center justify-center text-[14px] font-[500] bg-white cursor-pointer w-[156px]">Login To Access</button>
+        <button onClick={onLoginClick} className="sticky top-[50%] h-[36px] rounded-[8px] flex items-center justify-center text-[14px] font-[500] bg-white cursor-pointer w-[156px]">Login To Access</button>
         </div>
       </div>
     </div>
