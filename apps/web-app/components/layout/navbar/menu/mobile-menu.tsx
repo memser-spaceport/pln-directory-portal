8748dfd0858f82e2c415063c1683f39037e67163
Vellaiyan-Marimuthu <vellaiyan.m@ideas2it.com>
2024-04-12 16:25:44 +0530

    feat/irl-new-changes: mobile support added for navbar

--- apps/web-app/components/layout/navbar/menu/mobile-menu.tsx
+++ apps/web-app/components/layout/navbar/menu/mobile-menu.tsx
@@ -52,38 +52,54 @@ export function MobileMenu() {
   };
 
   return (
-    <ul className="flex w-full justify-between text-sm text-gray-700">
-      {MENU_ITEMS.map((item) => {
-        const Icon = item.icon;
+    <>
+      <ul className="flex w-full justify-between text-sm text-gray-700">
+        {MENU_ITEMS.map((item) => {
+          const Icon = item.icon;
 
-        return (
-          <li key={item.path}>
-            <Link href={item.path}>
-              <a
-                className={`on-focus h-[60px] group flex flex-col items-center rounded-lg px-3 py-2.5 text-sm hover:text-slate-900 focus:text-slate-900 ${
-                  router.asPath.includes(item.path)
-                    ? 'bg-slate-200 text-slate-900'
-                    : 'text-slate-600'
-                }`}
-                onClick={(e) => onMenuItemClicked(item.name)}
-              >
-                <div>
-                  <Icon
-                    data-testid={`${item.name}-icon`}
-                    className={` h-5 w-5 group-hover:fill-slate-900 ${
-                      router.asPath.includes(item.path)
-                        ? 'fill-slate-900'
-                        : 'fill-slate-600'
-                    }`}
-                  />
-                </div>
-                <span className="text-[10px]">{item.name}</span>
-                {item.name === 'IRL Gatherings' && <img src='/assets/images/icons/beta-logo.svg' alt='beta logo'/>}
-              </a>
-            </Link>
-          </li>
-        );
-      })}
-    </ul>
+          return (
+            <li key={item.path}>
+              <Link href={item.path}>
+                <a
+                  className={`on-focus group flex h-[60px] flex-col items-center rounded-lg px-3 py-2.5 text-sm hover:text-slate-900 focus:text-slate-900 ${
+                    router.asPath.includes(item.path)
+                      ? 'bg-slate-200 text-slate-900'
+                      : 'text-slate-600'
+                  }`}
+                  onClick={(e) => onMenuItemClicked(item.name)}
+                >
+                  <div>
+                    <Icon
+                      data-testid={`${item.name}-icon`}
+                      className={` h-5 w-5 group-hover:fill-slate-900 ${
+                        router.asPath.includes(item.path)
+                          ? 'fill-slate-900'
+                          : 'fill-slate-600'
+                      }`}
+                    />
+                  </div>
+                  <span className="mb-text text-[10px]">{item.name}</span>
+                  {item.name === 'IRL Gatherings' && (
+                    <img
+                      src="/assets/images/icons/beta-logo.svg"
+                      alt="beta logo"
+                    />
+                  )}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <style jsx>
+        {`
+          @media (max-width: 375px) {
+            .mb-text {
+              font-size: 8px;
+            }
+          }
+        `}
+      </style>
+    </>
   );
 }
