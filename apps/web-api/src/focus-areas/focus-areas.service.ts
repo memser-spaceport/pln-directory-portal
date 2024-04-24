dbac8fee9043f38b8ecad558493939ca144cc06d
Navaneethakrishnan <navaneeth@ideas2it.com>
2024-04-24 12:27:22 +0530

    revert: reverted search member by contributed project

--- apps/web-api/src/focus-areas/focus-areas.service.ts
+++ apps/web-api/src/focus-areas/focus-areas.service.ts
@@ -2,80 +2,65 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../shared/prisma.service';
 import { TeamsService } from '../teams/teams.service'; 
 import { ProjectsService } from '../projects/projects.service';
+import { PROJECT, TEAM } from '../utils/constants';
 @Injectable()
 export class FocusAreasService {
   constructor(private prisma: PrismaService, private teamsService: TeamsService, private projectService: ProjectsService) {}
 
-  async findAll(teamFilter, projectFilter) {
+  async findAll(query) {
+    const { type } = query;
     const result = await this.prisma.focusArea.findMany({
       include: {
-        children: this.buildQueryByLevel(4, teamFilter, projectFilter), // level denotes depth of children.
-        teamAncestorFocusAreas: {
-          where: {
-            team: {
-              ...teamFilter
-            }
-          },
-          distinct: "teamUid"
-        },
-        projectAncestorFocusAreas: {
-          where: {
-            project: {
-              ...projectFilter
-            }
-          },
-          distinct: "projectUid"
-        }
+        children: this.buildQueryByLevel(4, type, query), // level denotes depth of children.
+        ...this.buildAncestorFocusAreasFilterByType(type, query)
       }
     });
     return result;
   }
 
-  private buildQueryByLevel(level: number, teamFilter, projectFilter) {
+  private buildQueryByLevel(level: number, type, query) {
     if (level === 0) {
       return {
         include: {
           children: true,
-          teamAncestorFocusAreas: {
-            where: {
-              team: {
-                ...teamFilter
-              }
-            },
-            distinct: "teamUid"
-          },
-          projectAncestorFocusAreas: {
-            where: {
-              project: {
-                ...projectFilter
-              }
-            },
-            distinct: "projectUid"
-          }
+          ...this.buildAncestorFocusAreasFilterByType(type, query)
         }
       };
     }
     return {
       include: {
-        children: this.buildQueryByLevel(level - 1, teamFilter, projectFilter),
+        children: this.buildQueryByLevel(level - 1, type, query),
+        ...this.buildAncestorFocusAreasFilterByType(type, query)
+      }
+    };
+  }
+
+  buildAncestorFocusAreasFilterByType(type, query):any {
+    if (type === TEAM) {
+      return  {
         teamAncestorFocusAreas: {
           where: {
             team: {
-              ...teamFilter
+              ...this.buildTeamFilter(query)
             }
           },
           distinct: "teamUid"
-        },
+        }
+      }
+    }
+    if (type === PROJECT) {
+      return {
         projectAncestorFocusAreas: {
           where: {
             project: {
-              ...projectFilter
+              ...this.buildProjectFilter(query)
             }
           },
           distinct: "projectUid"
         }
       }
-    };
+    }
+    return {}
   }
   
   buildTeamFilter(queryParams) {
