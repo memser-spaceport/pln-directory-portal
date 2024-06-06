c321d6d5fe8a2014c083bee05d2e014ab8d5aac1
Thangaraj Esakky <thangaraj.esakky@ideas2it.com>
2024-06-06 15:33:06 +0530

    feat/privy: Fixed logout issue

--- apps/web-app/pages/settings.tsx
+++ apps/web-app/pages/settings.tsx
@@ -130,14 +130,17 @@ export default function Settings({
             const refreshTokenExpiry = decodeToken(refreshToken);
             Cookies.set('authToken', JSON.stringify(accessToken), { 
                 expires: new Date(accessTokenExpiry.exp * 1000),
+                domain: process.env.COOKIE_DOMAIN || '',
                
             });
             Cookies.set('refreshToken', JSON.stringify(refreshToken), {
                 expires: new Date(refreshTokenExpiry.exp * 1000),
+                domain: process.env.COOKIE_DOMAIN || '',
               
             });
             Cookies.set('userInfo', JSON.stringify(newUserInfo), { 
                 expires: new Date(refreshTokenExpiry.exp * 1000),
+                domain: process.env.COOKIE_DOMAIN || '',
             });
             document.dispatchEvent(new CustomEvent('app-loader-status'))
             toast.success('Email Updated Successfully')
