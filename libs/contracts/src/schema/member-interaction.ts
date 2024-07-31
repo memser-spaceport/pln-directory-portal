e742f0a2114539bf400035b268a3f66e9becf1c1
Navaneethakrishnan <navaneeth@ideas2it.com>
2024-07-31 18:14:29 +0530

    feat: added transation in member interaction, feedback, followup   - added migration files

--- libs/contracts/src/schema/member-interaction.ts
+++ libs/contracts/src/schema/member-interaction.ts
@@ -1,9 +1,14 @@
 import { z,  } from "zod";
 import { createZodDto } from '@abitia/zod-dto';
 
+export const MemberInteractionType = z.enum([
+  "SCHEDULE_MEETING"
+]);
+
 const MemberInteractionSchema = z.object({
   id: z.number().int(),
   uid: z.string(),
+  type: MemberInteractionType,
   data: z.any().optional(),
   hasFollowUp: z.boolean().optional(),
   createdAt: z.string(),
