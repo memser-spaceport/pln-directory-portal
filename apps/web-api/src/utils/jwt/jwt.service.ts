da335002d1e3c90ad8519f7c842460e0fe85731f
madan v <madan.v@ideas2it.com>
2024-10-22 13:17:48 +0530

    feat: improve docs for token service

--- apps/web-api/src/utils/jwt/jwt.service.ts
+++ apps/web-api/src/utils/jwt/jwt.service.ts
@@ -1,18 +1,29 @@
-/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
+
 @Injectable()
 export class JwtService {
-  async getSignedToken() {
+  
+  /**
+   * <p> Returns a signed jwt token for given roles valid for an hour </p>
+   * @returns the signed jwt token as string
+   */
+  async getSignedToken(roles: string[]) {
     return jwt.sign(
       {
-        roles: ['DIRECTORYADMIN'],
+        roles,
         exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
       },
       process.env.ADMIN_TOKEN_SECRET
     );
   }
 
+  /**
+   * <p>Validates a given jwt token based on the secret used to sign</p>
+   * @param token the jwt token as string to validate
+   * @returns 
+   * @throws if token expired or invalid
+   */
   async validateToken(token) {
     return jwt.verify(token, process.env.ADMIN_TOKEN_SECRET);
   }
