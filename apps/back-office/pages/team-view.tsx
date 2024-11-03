7a6802fc800a2e2369f1e798b87068de081bca86
Vellaiyan-Marimuthu <vellaiyan.m@ideas2it.com>
2024-11-03 16:14:15 +0530

    Feat: Participation request modularization

--- apps/back-office/pages/team-view.tsx
+++ apps/back-office/pages/team-view.tsx
@@ -169,7 +169,7 @@ export default function TeamView(props) {
       requestId: props.id,
     };
     api
-      .post(`/v1/participants-request/unique-identifier`, data)
+      .get(`/v1/participants-request/unique-identifier?type=${data?.participantType}&identifier=${data.uniqueIdentifier}`)
       .then((response) => {
         setDisableSave(false);
         response?.data &&
