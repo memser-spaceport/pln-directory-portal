2c59e47d6652783109346b3ab4e3e6206aaf80bc
Navaneethakrishnan <navaneeth@ideas2it.com>
2024-11-05 17:03:39 +0530

    feat: added seed data for discovery question   - fixed isuse slack notification config

--- apps/web-api/src/utils/slack/slack.service.ts
+++ apps/web-api/src/utils/slack/slack.service.ts
@@ -6,8 +6,12 @@ import axios from 'axios';
 export class SlackService {
   async notifyToChannel({ requestLabel, url, name }) {
     try {
+      const channel = process.env.CHANNEL_ID;
+      const authToken =  process.env.SLACK_BOT_TOKEN;
+      if (!channel || !authToken) {
+        return null;
+      }
       const content = `${requestLabel} : ${name} \n${url}`;
-
       const slackResponse = await axios({
         method: 'post',
         url: 'https://slack.com/api/chat.postMessage',
