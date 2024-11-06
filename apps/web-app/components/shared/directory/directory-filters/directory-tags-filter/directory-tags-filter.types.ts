52795184771d969240eadc4184d888bd6f68392d
yosuva Rajendran <yosuva.rajendran@ideas2it.com>
2024-11-06 16:18:40 +0530

    feat: remove web-app files

--- apps/web-app/components/shared/directory/directory-filters/directory-tags-filter/directory-tags-filter.types.ts
+++
@@ -1,6 +0,0 @@
-export interface IFilterTag {
-  label?: string;
-  value: string;
-  selected: boolean;
-  disabled: boolean;
-}
--- apps/web-app/components/shared/social-links/social-link/social-link.types.ts
+++
@@ -1,6 +0,0 @@
-export interface ISocialLink {
-  link?: string;
-  label?: string;
-}
-
-export type TSocialLinkType = 'email' | 'twitter' | 'github' | 'linkedin' | 'telegram' | 'discord';
