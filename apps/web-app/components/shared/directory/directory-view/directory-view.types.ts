528a0a16ad2ab0414af294f72f60f995231c4a24
yosuva Rajendran <yosuva.rajendran@ideas2it.com>
2024-11-06 16:18:40 +0530

    feat: remove web-app files

--- apps/web-app/components/shared/directory/directory-view/directory-view.types.ts
+++
@@ -1,8 +0,0 @@
-export const viewTypes = ['grid', 'list'] as const;
-export type TViewType = typeof viewTypes[number];
-
-export interface IDirectoryViewTypeOption {
-  viewType: TViewType;
-  icon: React.ComponentType<React.SVGProps<SVGSVGElement>>;
-  label: string;
-}
