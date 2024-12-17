e43c3c5dfa100b4cf1986102e2921966a6feb1b1
navneethkrish <navaneeth@ideas2it.com>
2024-12-17 13:08:26 +0530

    fix(app module): included member-follows module in app module

--- apps/web-api/src/member-follows/member-follows.module.ts
+++ apps/web-api/src/member-follows/member-follows.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MemberFollowsService } from './member-follows.service';
 import { MemberFollowsController } from './member-follows.controller';
+import { MembersModule } from '../members/members.module';
 
 @Module({
   controllers: [MemberFollowsController],
   providers: [MemberFollowsService],
-  exports: [MemberFollowsService]
+  exports: [MemberFollowsService],
+  imports:[MembersModule]
 })
 export class MemberFollowsModule {}
