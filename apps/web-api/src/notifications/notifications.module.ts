1ce18a3743b75de6d857ffd1d0ce9060cdaee088
Navaneethakrishnan <navaneeth@ideas2it.com>
2024-12-23 13:53:44 +0530

    fix: fixed issue in member subscription

--- apps/web-api/src/notifications/notifications.module.ts
+++ apps/web-api/src/notifications/notifications.module.ts
@@ -1,5 +1,6 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { NotificationService } from './notifications.service';
+import { NotificationConsumer } from './notifications.processor';
 import { BullModule } from '@nestjs/bull';
 import { SharedModule } from '../shared/shared.module';
 import { MemberSubscriptionsModule } from '../member-subscriptions/member-subscriptions.module';
@@ -7,7 +8,7 @@ import { PLEventsModule } from '../pl-events/pl-events.module';
 
 @Module({
   controllers: [],
-  providers: [NotificationService],
+  providers: [NotificationService, NotificationConsumer],
   exports: [NotificationService],
   imports: [
     SharedModule,
