c7b2e799ac57701cce5a3dd6cceb6b5e0d79c8af
navneethkrish <navaneeth@ideas2it.com>
2024-12-31 15:35:14 +0530

    fix(cache revalidation): added revalidation tag for team airtable

--- apps/web-api/src/utils/cache/cache.service.ts
+++ apps/web-api/src/utils/cache/cache.service.ts
@@ -12,11 +12,11 @@ export class CacheService {
 
   // Mapping service names to tags
   private serviceTagsMap = {
-    members: ['member-filters', 'member-list', 'members-roles', "featured", "member-airtable", "member-repositories", "member-detail"],
+    members: ['member-filters', 'member-list', 'members-roles', "featured", "member-airtable", "member-repositories", "member-detail","team-list"],
     projects: ['project-list', 'focus-areas', "project-detail", "team-detail", "featured", "project-oso"],
-    teams: ['team-filters', 'team-list', 'focus-areas', "team-detail", "featured"],
+    teams: ['team-filters', 'team-list', 'focus-areas', "team-detail", "featured", "team-airtable"],
     'participants-requests': ['member-filters', 'member-list', 'team-filters', 'team-list', 'focus-areas'],
-    PLEventGuest: ["irl-locations", "irl-guests", "irl-locations-topic", "irl-guest-events", "member-preferences"]
+    PLEventGuest: ["irl-locations", "irl-guests", "irl-locations-topic", "irl-guest-events"]
   };
 
   // Reset cache and call API based on service
