96ad52a8fc12f3d4b95b62839bd70387b75e8a57
Thangaraj Esakky <thangaraj.esakky@ideas2it.com>
2025-01-02 00:53:49 +0530

    feat(husky): added endpoint for husky chat

--- apps/web-api/src/husky/husky.module.ts
+++ apps/web-api/src/husky/husky.module.ts
@@ -1,14 +1,15 @@
 import { Module } from '@nestjs/common';
 import { HuskyService } from './husky.service';
+import { HuskyController } from './husky.controller';
+import { RedisCacheDbService } from './db/redis-cache-db.service';
+import { QdrantVectorDbService } from './db/qdrant-vector-db.service';
+import { MongoPersistantDbService } from './db/mongo-persistant-db.service';
+import { Neo4jGraphDbService } from './db/neo4j-graph-db.service';
 
 @Module({
-  controllers: [],
-  providers: [
-    HuskyService
-  ],
-  imports:[],
-  exports: [
-    HuskyService
-  ]
+  controllers: [HuskyController],
+  providers: [HuskyService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, Neo4jGraphDbService],
+  imports: [],
+  exports: [HuskyService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, Neo4jGraphDbService],
 })
-export class  HuskyModule {}
+export class HuskyModule {}
