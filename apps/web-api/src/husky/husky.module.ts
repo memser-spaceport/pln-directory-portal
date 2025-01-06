aa92e87f6e887ae73b933e920fd886ec2bf6f1b6
Thangaraj Esakky <thangaraj.esakky@ideas2it.com>
2025-01-06 08:06:52 +0530

    feat(husky): modified ai prompts and consolidated ai logics to husky ai service

--- apps/web-api/src/husky/husky.module.ts
+++ apps/web-api/src/husky/husky.module.ts
@@ -5,11 +5,26 @@ import { RedisCacheDbService } from './db/redis-cache-db.service';
 import { QdrantVectorDbService } from './db/qdrant-vector-db.service';
 import { MongoPersistantDbService } from './db/mongo-persistant-db.service';
 import { Neo4jGraphDbService } from './db/neo4j-graph-db.service';
+import { HuskyAiService } from './husky-ai.service';
 
 @Module({
   controllers: [HuskyController],
-  providers: [HuskyService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, Neo4jGraphDbService],
+  providers: [
+    HuskyService,
+    HuskyAiService,
+    RedisCacheDbService,
+    QdrantVectorDbService,
+    MongoPersistantDbService,
+    Neo4jGraphDbService,
+  ],
   imports: [],
-  exports: [HuskyService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, Neo4jGraphDbService],
+  exports: [
+    HuskyService,
+    HuskyAiService,
+    RedisCacheDbService,
+    QdrantVectorDbService,
+    MongoPersistantDbService,
+    Neo4jGraphDbService,
+  ],
 })
 export class HuskyModule {}
