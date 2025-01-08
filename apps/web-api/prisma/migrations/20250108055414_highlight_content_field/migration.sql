7594e2f0b8a56c94a2d196bf4f1676855224bb35
NivedhaSV <nivedha.selvaraja@ideas2it.com>
2025-01-08 11:26:14 +0530

    feat(highlight-field): new highlight field added to Team and Project table

---
+++ apps/web-api/prisma/migrations/20250108055414_highlight_content_field/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Project" ADD COLUMN     "highlightContent" JSONB;
+
+-- AlterTable
+ALTER TABLE "Team" ADD COLUMN     "highlightContent" JSONB;
--- apps/web-api/src/teams/teams.service.ts
+++ apps/web-api/src/teams/teams.service.ts
@@ -265,7 +265,7 @@ export class TeamsService {
       'name', 'blog', 'contactMethod', 'twitterHandler',
       'linkedinHandler', 'telegramHandler', 'officeHours',
       'shortDescription', 'website', 'airtableRecId',
-      'longDescription', 'moreDetails'
+      'longDescription', 'moreDetails', 'highlightContent'
     ];
     copyObj(teamData, team, directFields);
     // Handle one-to-one or one-to-many mappings
