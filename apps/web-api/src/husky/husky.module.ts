41d4bc4053d6276c3841a8372b2740021ab2cea2
Thangaraj Esakky <thangaraj.esakky@ideas2it.com>
2025-02-23 18:35:32 +0530

    feat(husky-text-to-sql): added new api path to support text to sql

--- apps/web-api/src/husky/husky.module.ts
+++ apps/web-api/src/husky/husky.module.ts
@@ -4,13 +4,13 @@ import { HuskyController } from './husky.controller';
 import { RedisCacheDbService } from './db/redis-cache-db.service';
 import { QdrantVectorDbService } from './db/qdrant-vector-db.service';
 import { MongoPersistantDbService } from './db/mongo-persistant-db.service';
-import { Neo4jGraphDbService } from './db/neo4j-graph-db.service';
 import { HuskyAiService } from './husky-ai.service';
+import { PostgresSqlDb } from './db/postgress-sql-db.service';
 
 @Module({
   controllers: [HuskyController],
-  providers: [HuskyService, HuskyAiService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService],
+  providers: [HuskyService, HuskyAiService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, PostgresSqlDb],
   imports: [],
-  exports: [HuskyService, HuskyAiService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService],
+  exports: [HuskyService, HuskyAiService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, PostgresSqlDb],
 })
 export class HuskyModule {}
