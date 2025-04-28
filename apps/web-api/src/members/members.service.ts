8b175afaf9f8b2f489fdb40e4d6f5f71e23486d3
ramkumar-loganathan <ramkumar.loganathan@ideas2it.com>
2025-04-28 11:32:45 +0530

    feat: introduced new question type in dicovery section

--- apps/web-api/src/members/members.service.ts
+++ apps/web-api/src/members/members.service.ts
@@ -1542,7 +1542,10 @@ export class MembersService {
       }
     } else if (error instanceof Prisma.PrismaClientValidationError) {
       throw new BadRequestException('Database field validation error on Member', error.message);
+    } else {
+      throw error;
     }
+    // TODO: Remove this return statement if future versions allow all error-returning functions to be inferred correctly.
     return error;
   }
 
