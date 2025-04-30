3f35ae5bc4e99840de8813da1777711b25d10fe1
vova <vova.horin@magicpowered.io>
2025-04-30 21:14:14 +0300

    feat: add asks swagger schema

--- apps/web-api/src/asks/asks.controller.ts
+++ apps/web-api/src/asks/asks.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, UseGuards, Body, Param, Req, HttpStatus, HttpCode, UsePipes } from '@nestjs/common';
 import { Api, initNestServer } from '@ts-rest/nest';
 import { ZodValidationPipe } from '@abitia/zod-dto';
+import { AskStatus } from '@prisma/client';
+import { ApiParam, ApiTags } from '@nestjs/swagger';
 import { apiAsks } from 'libs/contracts/src/lib/contract-asks';
 import { AskService } from './asks.service';
 import { UserTokenValidation } from '../guards/user-token-validation.guard';
@@ -10,21 +12,27 @@ import {
   ResponseAskDto,
   ResponseAskWithRelationsDto,
   CloseAskDto,
+  CreateAskSchema,
+  UpdateAskSchema,
+  CloseAskSchema,
 } from 'libs/contracts/src/schema/ask';
-import { AskStatus } from '@prisma/client';
+import { ApiBodyFromZod } from '../decorators/api-body-from-zod';
 
 const server = initNestServer(apiAsks);
 
+@ApiTags('Asks')
 @Controller()
 export class AsksController {
   constructor(private readonly askService: AskService) {}
 
   @Api(server.route.getAsk)
+  @ApiParam({ name: 'uid', type: 'string' })
   findOne(@Param('uid') uid: string): Promise<ResponseAskWithRelationsDto> {
     return this.askService.findOne(uid);
   }
 
   @Api(server.route.createTeamAsk)
+  @ApiBodyFromZod(CreateAskSchema)
   @UseGuards(UserTokenValidation)
   @UsePipes(ZodValidationPipe)
   async createForTeam(
@@ -36,6 +44,7 @@ export class AsksController {
   }
 
   @Api(server.route.updateAsk)
+  @ApiBodyFromZod(UpdateAskSchema)
   @UseGuards(UserTokenValidation)
   @UsePipes(ZodValidationPipe)
   async update(@Param('uid') uid: string, @Body() updateAskDto: UpdateAskDto, @Req() req): Promise<ResponseAskDto> {
@@ -43,6 +52,7 @@ export class AsksController {
   }
 
   @Api(server.route.closeAsk)
+  @ApiBodyFromZod(CloseAskSchema)
   @UseGuards(UserTokenValidation)
   @UsePipes(ZodValidationPipe)
   async close(@Param('uid') uid: string, @Body() closeAskDto: CloseAskDto, @Req() req): Promise<ResponseAskDto> {
@@ -55,6 +65,7 @@ export class AsksController {
   }
 
   @Api(server.route.deleteAsk)
+  @ApiParam({ name: 'uid', type: 'string' })
   @UseGuards(UserTokenValidation)
   @HttpCode(HttpStatus.NO_CONTENT)
   async delete(@Param('uid') uid: string, @Req() req): Promise<boolean> {
