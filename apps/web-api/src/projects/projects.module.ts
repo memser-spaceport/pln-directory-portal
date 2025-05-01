649bc0153b4ddc5f984972e168b86fa95c4c7ce7
vova <vova.horin@magicpowered.io>
2025-05-01 18:58:58 +0300

    fix: remove ask service from projects providers

--- apps/web-api/src/projects/projects.module.ts
+++ apps/web-api/src/projects/projects.module.ts
@@ -3,13 +3,12 @@ import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
 import { MembersModule } from '../members/members.module';
 import { AskModule } from '../asks/asks.module';
-import { AskService } from '../asks/asks.service';
 import { HuskyModule } from '../husky/husky.module';
 
 @Module({
   imports: [MembersModule, AskModule, HuskyModule],
   controllers: [ProjectsController],
-  providers: [ProjectsService, AskService],
-  exports: [ProjectsService]
+  providers: [ProjectsService],
+  exports: [ProjectsService],
 })
 export class ProjectsModule {}
