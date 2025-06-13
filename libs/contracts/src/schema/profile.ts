7607c5c275d0c44083142f830baeb4d30891f684
Stanislav Parkhomenko <stanislav.parkhomenko@gmail.com>
2025-06-13 16:05:56 +0300

    feat(lab-206): added algorithm for profile completion calculation

--- libs/contracts/src/schema/profile.ts
+++ libs/contracts/src/schema/profile.ts
@@ -4,6 +4,14 @@ import { createZodDto } from '@abitia/zod-dto';
 export const ProfileCompletenessResponseSchema = z.object({
   memberUid: z.string(),
   completeness: z.number(),
+  sections: z.object({
+    profileDetails: z.number(),
+    contactDetails: z.number(),
+    teamRole: z.number(),
+    experience: z.number(),
+    repositories: z.number(),
+    projectContributions: z.number(),
+  })
 });
 
 export class ProfileCompletenessResponse extends createZodDto(ProfileCompletenessResponseSchema) {}
