28b902f3cdaa1bbbf8f7b7cb4d35123c9641cd66
vova <vova.horin@magicpowered.io>
2025-07-12 14:52:47 +0300

    fix: consolidate token extraction logic into a utility function across multiple guards

--- apps/web-api/src/guards/user-access-token-validate.guard.ts
+++ apps/web-api/src/guards/user-access-token-validate.guard.ts
@@ -7,6 +7,7 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import axios from 'axios';
+import { extractTokenFromRequest } from '../utils/auth';
 
 @Injectable()
 export class UserAccessTokenValidateGuard implements CanActivate {
@@ -14,7 +15,7 @@ export class UserAccessTokenValidateGuard implements CanActivate {
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
-    const token = this.extractTokenFromHeader(request);
+    const token = extractTokenFromRequest(request);
 
     // If no token throw Exception
     if (!token) {
@@ -23,10 +24,7 @@ export class UserAccessTokenValidateGuard implements CanActivate {
 
     // If token validation fails throw Exception
     try {
-      const validationResult: any = await axios.post(
-        `${process.env.AUTH_API_URL}/auth/introspect`,
-        { token: token }
-      );
+      const validationResult: any = await axios.post(`${process.env.AUTH_API_URL}/auth/introspect`, { token: token });
       if (!validationResult?.data?.active) {
         throw new UnauthorizedException('Invalid Session. Please login and try again');
       }
@@ -35,25 +33,17 @@ export class UserAccessTokenValidateGuard implements CanActivate {
       if (validationResult?.data?.email) {
         request['userEmail'] = validationResult.data.email;
         request['userUid'] = validationResult.data.sub;
-        request['userAccessToken'] = token
+        request['userAccessToken'] = token;
       }
     } catch (error) {
       // If known error, handle it
       if (error?.response?.data?.message && error?.response?.status) {
-        throw new HttpException(
-          error?.response?.data?.message,
-          error?.response?.status
-        );
+        throw new HttpException(error?.response?.data?.message, error?.response?.status);
       } else if (error?.response?.message && error?.status) {
         throw new HttpException(error?.response?.message, error?.status);
       }
       throw new InternalServerErrorException('Unexpected Error');
     }
     return true;
-  }
-
-  private extractTokenFromHeader(request): string | undefined {
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    return type === 'Bearer' ? token : undefined;
   }
 }
