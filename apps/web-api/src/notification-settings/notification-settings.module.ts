cb6e99a8ad52fa478dcfb2ff0402e74febcb9f1b
vova <vova.horin@magicpowered.io>
2025-07-21 14:32:04 +0300

    feat: implement notification service client and enhance notification settings functionality

--- apps/web-api/src/notification-settings/notification-settings.module.ts
+++ apps/web-api/src/notification-settings/notification-settings.module.ts
@@ -4,9 +4,15 @@ import { NotificationSettingsService } from './notification-settings.service';
 import { SharedModule } from '../shared/shared.module';
 import { MembersModule } from '../members/members.module';
 import { RecommendationsModule } from '../recommendations/recommendations.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [SharedModule, RecommendationsModule, forwardRef(() => MembersModule)],
+  imports: [
+    SharedModule,
+    RecommendationsModule,
+    forwardRef(() => MembersModule),
+    forwardRef(() => NotificationsModule),
+  ],
   controllers: [NotificationSettingsController],
   providers: [NotificationSettingsService],
   exports: [NotificationSettingsService],
