d786b768f56ec05088f961dff9e823b4c53566ca
madan v <madan.v@ideas2it.com>
2025-07-27 17:09:04 +0530

    fix: removed duplicate prisma service initialize

--- apps/web-api/src/husky/husky.module.ts
+++ apps/web-api/src/husky/husky.module.ts
@@ -6,15 +6,15 @@ import { QdrantVectorDbService } from './db/qdrant-vector-db.service';
 import { MongoPersistantDbService } from './db/mongo-persistant-db.service';
 import { HuskyAiService } from './husky-ai.service';
 import { HuskyThreadsController } from './husky-threads.controller';
-import { PrismaService } from '../shared/prisma.service';
 import { HuskyRevalidationService } from './husky-revalidation.service';
 import { HuskyAiToolsModule } from './tools/husky-ai-tools.module';
 import { HuskyGenerationService } from './husky-generation.service';
 import { HuskyGenerationController } from './husky-generation.controller';
+import { SharedModule } from '../shared/shared.module';
 @Module({
   controllers: [HuskyChatsController, HuskyThreadsController, HuskyGenerationController],
-  providers: [HuskyService, HuskyAiService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, PrismaService, HuskyRevalidationService, HuskyGenerationService],
-  imports: [HuskyAiToolsModule],
+  providers: [HuskyService, HuskyAiService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, HuskyRevalidationService, HuskyGenerationService],
+  imports: [SharedModule, HuskyAiToolsModule],
   exports: [HuskyService, HuskyAiService, RedisCacheDbService, QdrantVectorDbService, MongoPersistantDbService, HuskyRevalidationService, HuskyGenerationService,],
 })
 export class HuskyModule {}
