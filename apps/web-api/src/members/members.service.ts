ca1e80a2b30b9ffdb2e97c17d60f93b93016a526
tasha <natakravchenko.nk@gmail.com>
2025-08-21 15:16:54 +0300

    feat: oh activity data - Add scheduleMeetingCount to members-search response

--- apps/web-api/src/members/members.service.ts
+++ apps/web-api/src/members/members.service.ts
@@ -1808,6 +1808,7 @@ export class MembersService {
             ohInterest: true,
             ohHelpWith: true,
             openToWork: true,
+            scheduleMeetingCount: true,
             image: {
               select: {
                 uid: true,
