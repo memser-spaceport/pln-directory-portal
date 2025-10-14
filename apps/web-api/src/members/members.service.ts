7a7c1d3b78a1bb700cf784be146a4f495e68180d
Mykyta Shevelov <nick.shevelov@magicpowered.io>
2025-10-14 11:40:21 -0300

    LAB-895 Update Investor filter on member page (#2643)
    * feat: improve investor's filter
    
    * feat: add ANGEL filter type

--- apps/web-api/src/members/members.service.ts
+++ apps/web-api/src/members/members.service.ts
@@ -2009,6 +2009,72 @@ export class MembersService {
               },
             ],
           },
+          // If InvestorProfile.type = 'FUND', member must belong to a team with isFund = true
+          {
+            OR: [
+              // Allow if not FUND type
+              {
+                investorProfile: {
+                  OR: [{ type: { not: 'FUND' } }, { type: { equals: null } }],
+                },
+              },
+              // Or if FUND type, require member to be in a team with isFund = true
+              {
+                AND: [
+                  {
+                    investorProfile: {
+                      type: 'FUND',
+                    },
+                  },
+                  {
+                    teamMemberRoles: {
+                      some: {
+                        team: {
+                          isFund: true,
+                        },
+                      },
+                    },
+                  },
+                ],
+              },
+            ],
+          },
+          // If InvestorProfile.type = 'ANGEL', at least one field must be filled
+          {
+            OR: [
+              // Allow if not ANGEL type
+              {
+                investorProfile: {
+                  OR: [{ type: { not: 'ANGEL' } }, { type: { equals: null } }],
+                },
+              },
+              // Or if ANGEL type, at least one field must be filled
+              {
+                AND: [
+                  {
+                    investorProfile: {
+                      type: 'ANGEL',
+                    },
+                  },
+                  {
+                    investorProfile: {
+                      OR: [
+                        {
+                          investInStartupStages: { isEmpty: false },
+                        },
+                        {
+                          typicalCheckSize: { not: null, gt: 0 },
+                        },
+                        {
+                          investmentFocus: { isEmpty: false },
+                        },
+                      ],
+                    },
+                  },
+                ],
+              },
+            ],
+          },
         ],
       });
     }
