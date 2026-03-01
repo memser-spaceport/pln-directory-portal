86397766eef7f25e16d3631eab18a1900c359233
tasha <natakravchenko.nk@gmail.com>
2026-03-02 00:43:21 +0200

    fix: fix duplicate Prisma execution in middleware and stabilize DB event tracking

--- apps/web-api/src/metrics/metrics.controller.ts
+++ apps/web-api/src/metrics/metrics.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { register, collectDefaultMetrics } from 'prom-client';
+import {NoCache} from "../decorators/no-cache.decorator";
 
 collectDefaultMetrics();
 
 @Controller('metrics')
 export class MetricsController {
   @Get()
+  @NoCache()
   async getMetrics(@Res() res) {
     res.set('Content-Type', register.contentType);
     res.end(await register.metrics());
