5dc69099706b7e3f0965d1dc09e8436d9941a56a
tasha <natakravchenko.nk@gmail.com>
2026-07-06 17:39:40 +0300

    Remove Heroku dependency from health check

--- apps/web-api/src/health/health.module.ts
+++ apps/web-api/src/health/health.module.ts
@@ -2,12 +2,11 @@ import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
 import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
-import { HerokuHealthIndicator } from './heroku.health';
 import { PrismaHealthIndicator } from './prisma.health';
 
 @Module({
   controllers: [HealthController],
   imports: [TerminusModule, HttpModule],
-  providers: [PrismaHealthIndicator, HerokuHealthIndicator],
+  providers: [PrismaHealthIndicator],
 })
 export class HealthModule {}
