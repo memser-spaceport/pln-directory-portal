433cedd1fde35fe48bb32973c82ad1e7270c46bf
Mykyta Shevelov <nikita.shevelov@frame.xyz>
2026-09-04 13:04:11 -0300

    feat: restore AI Search using Gemini

--- apps/web-api/src/husky/db/husky-db.interface.ts
+++ apps/web-api/src/husky/db/husky-db.interface.ts
@@ -3,15 +3,6 @@ export interface HuskyCacheDbService {
   get<T>(key: string): Promise<T | null>;
 }
 
-export interface HuskyVectorDbService<T> {
-  searchEmbeddings(
-    collectionName: string,
-    embeddingToMatch: number[],
-    limit: number,
-    with_payload: boolean
-  ): Promise<T[]>;
-}
-
 export interface HuskyGraphDbService<T> {
   query(cypher: string, params: any): Promise<any>;
 }
